85fb2807579418da0cac893fbc5ebd55119aba5b
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-01-02 10:32:57 +0530

    add index route
    commit to add index route

--- routes/web/routes.php
+++ routes/web/routes.php
@@ -4,6 +4,8 @@
  */
 
 use DT\Launcher\Controllers\HelloController;
+use DT\Launcher\Controllers\HomeController;
 use DT\Launcher\FastRoute\RouteCollector;
 
 $r->get( 'dt/launcher/hello', HelloController::class . '@show' );
+$r->get( 'dt/launcher/index', HomeController::class . '@index' );
