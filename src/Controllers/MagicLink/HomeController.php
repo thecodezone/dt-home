9cf9621593ca11fbfe15c5a0a1ce78db91849221
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-01-09 14:16:57 +0530

    add index route
    commit to add index route

--- src/Controllers/MagicLink/HomeController.php
+++ src/Controllers/MagicLink/HomeController.php
@@ -2,20 +2,25 @@
 
 namespace DT\Launcher\Controllers\MagicLink;
 
+use DT\Launcher\Illuminate\Http\RedirectResponse;
 use DT\Launcher\Illuminate\Http\Request;
 use DT\Launcher\Illuminate\Http\Response;
 use function DT\Launcher\magic_url;
-use function DT\Launcher\template;
+use function DT\Launcher\view;
 
 class HomeController {
 	public function show( Request $request, Response $response, $key ) {
-		$user        = wp_get_current_user();
+
+        $user        = wp_get_current_user();
 		$subpage_url = magic_url( 'subpage', $key );
 
-		return template( 'index', compact(
-			'user',
-			'subpage_url'
-		) );
+        $require_login = get_option('require_login');
+
+        if ($require_login === 1) {
+            return view( 'index' , compact('user', 'subpage_url'));
+        }else{
+            return new RedirectResponse( home_url('launcher/login') );
+        }
 	}
 
 	public function data( Request $request, Response $response, $key ) {
