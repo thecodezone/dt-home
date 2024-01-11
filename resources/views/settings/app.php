bbb45bf2c1b43519beab8f5824ed7e5d9caed844
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-01-11 18:40:25 +0530

    investigate-dt-icon-field
    commit to investigate dt icon field

---
+++ resources/views/settings/app.php
@@ -0,0 +1,69 @@
+<?php
+$this->layout('layouts/settings', compact('tab', 'link', 'page_title'))
+?>
+
+<form method="post">
+    <?php wp_nonce_field('dt_admin_form', 'dt_admin_form_nonce') ?>
+
+    <!-- Add a form -->
+</form>
+
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                    <span style="float:right;">
+                        <a href="admin.php?page=dt_launcher&tab=app&action=create" class="button float-right"><i
+                                class="fa fa-plus"></i> Add App</a>
+                    </span>
+
+                <br><br>
+
+                <table class="widefat striped" style="border-collapse: collapse; width: 100%;">
+                    <thead>
+                    <tr>
+                        <th style="border: 1px solid #ddd;">Name</th>
+                        <th style="border: 1px solid #ddd;">Type</th>
+                        <th style="border: 1px solid #ddd;">Icon</th>
+                        <th style="border: 1px solid #ddd;">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($data as $app) : ?>
+                        <tr>
+                            <td style="border: 1px solid #ddd;"><?php echo esc_html($app['name']); ?></td>
+                            <td style="border: 1px solid #ddd;"><?php echo esc_html($app['type']); ?></td>
+                            <td style="border: 1px solid #ddd;">
+                                <?php if (!empty($app['icon'])) : ?>
+                                    <img src="<?php echo esc_url($app['icon']); ?>" alt="Icon"
+                                         style="width: 50px; height: 50px;">
+                                <?php endif; ?>
+                            </td>
+
+                            <td style="border: 1px solid #ddd;">
+                                <a href="admin.php?page=dt_launcher&tab=app&action=up/<?php echo esc_attr($app['id']); ?>">Up</a>|
+                                <a href="admin.php?page=dt_launcher&tab=app&action=edit/<?php echo esc_attr($app['id']); ?>">Edit</a>|
+
+                                <?php if ($app['is_hidden'] == 1) { ?>
+                                    <a href="admin.php?page=dt_launcher&tab=app&action=unhide/<?php echo esc_attr($app['id']); ?>">Unhide</a>|
+                                <?php } else { ?>
+                                    <a href="admin.php?page=dt_launcher&tab=app&action=hide/<?php echo esc_attr($app['id']); ?>">Hide</a>|
+                                <?php } ?>
+
+                                <a href="admin.php?page=dt_launcher&tab=app&action=down/<?php echo esc_attr($app['id']); ?>">Down</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->start('right') ?>
+
+<!-- Add some content to the right side -->
+
+<?php $this->stop() ?>
