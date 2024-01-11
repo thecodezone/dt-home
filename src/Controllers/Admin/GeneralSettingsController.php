fdf006e5ebb146708f7ea67801bb8466174f729c
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-01-11 18:43:05 +0530

    Update all get_option checks

--- src/Controllers/Admin/GeneralSettingsController.php
+++ src/Controllers/Admin/GeneralSettingsController.php
@@ -7,38 +7,42 @@ use DT\Launcher\Illuminate\Http\Request;
 use DT\Launcher\Illuminate\Http\Response;
 use function DT\Launcher\view;
 
-class GeneralSettingsController {
-	/**
-	 * Show the general settings admin tab
-	 */
-	public function show( Request $request, Response $response ) {
-		$tab        = "general";
-		$link       = 'admin.php?page=dt_launcher&tab=';
-		$page_title = "Launcher Settings";
+class GeneralSettingsController
+{
+    /**
+     * Show the general settings admin tab
+     */
+    public function show(Request $request, Response $response)
+    {
+        $tab = "general";
+        $link = 'admin.php?page=dt_launcher&tab=';
+        $page_title = "Launcher Settings";
 
-		return view( "settings/general", compact( 'tab', 'link', 'page_title' ) );
-	}
+        return view("settings/general", compact('tab', 'link', 'page_title'));
+    }
 
-	/**
-	 * Submit the general settings admin tab form
-	 */
-	public function update( Request $request, Response $response ) {
+    /**
+     * Submit the general settings admin tab form
+     */
+    public function update(Request $request, Response $response)
+    {
 
-		// Add the settings update code here
+        // Add the settings update code here
 
-		return new RedirectResponse( 302, admin_url( 'admin.php?page=dt_launcher&tab=general&updated=true' ) );
+        return new RedirectResponse(302, admin_url('admin.php?page=dt_launcher&tab=general&updated=true'));
 
-	}
+    }
 
-	public function update_user_access_settings(Request $request, Response $response) {
+    public function update_user_access_settings(Request $request, Response $response)
+    {
 
-        $require_user = isset($_POST['require_user']) ? true : false;
+        $require_user = isset($_POST['dt_launcher_require_login']) ? true : false;
 
-			update_option('require_user', $require_user);
+        update_option('dt_launcher_require_login', $require_user);
 
-			$redirect_url = add_query_arg('message', 'updated', admin_url('admin.php?page=dt_launcher'));
+        $redirect_url = add_query_arg('message', 'updated', admin_url('admin.php?page=dt_launcher'));
 
-			return new RedirectResponse($redirect_url);
+        return new RedirectResponse($redirect_url);
 
-	}
+    }
 }
