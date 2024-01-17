d38106b8dd96104114cf0b6a6992c7f117368940
Craig Wann <craig@codezone.io>
2024-01-17 13:04:11 -0600

    Redirecting guest users to launcher login.

--- src/Middleware/LoggedIn.php
+++ src/Middleware/LoggedIn.php
@@ -6,15 +6,14 @@ use DT\Launcher\CodeZone\Router\Middleware\Middleware;
 use DT\Launcher\Illuminate\Http\RedirectResponse;
 use DT\Launcher\Illuminate\Http\Request;
 use DT\Launcher\Symfony\Component\HttpFoundation\Response;
-use function DT\Launcher\view;
 
 class LoggedIn implements Middleware {
 	public function handle( Request $request, Response $response, $next ) {
 
-		$require_login = get_option('dt_launcher_require_login');
+		$require_login = get_option( 'dt_launcher_require_login' );
 
-		if ( ! is_user_logged_in() && $require_login === 1) {
-			$response = new RedirectResponse( wp_login_url( $request->getUri() ), 302 );
+		if ( ! is_user_logged_in() && $require_login === 1 ) {
+			$response = new RedirectResponse( "/launcher/login", 302 );
 		}
 
 		return $next( $request, $response );
