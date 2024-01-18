596b8824be2328cc4b7a8392031449a3e5158814
Craig Wann <craig@codezone.io>
2024-01-18 13:59:56 -0600

    Fixing incorrect form action.

--- resources/views/settings/create.php
+++ resources/views/settings/create.php
@@ -57,7 +57,7 @@ if ( is_dir( $svgDirPath ) ) {
         cursor: pointer;
     }
 </style>
-<form action="admin.php?page=dt_launcher&tab=app" method="post" enctype="multipart/form-data">
+<form action="admin.php?page=dt_launcher&tab=app&action=create" method="post" enctype="multipart/form-data">
 	<?php wp_nonce_field( 'dt_admin_form', 'dt_admin_form_nonce' ) ?>
 
     <table class="widefat striped" id="ml_email_main_col_config">
