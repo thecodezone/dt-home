30784be344c06d9b0fc5ffe31ab0b8fff6dc3a4a
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-01-30 18:09:04 +0530

    Share link field
    commite to Share link field

--- resources/views/layouts/plugin.php
+++ resources/views/layouts/plugin.php
@@ -2,11 +2,21 @@
     <h1><?php $this->esc_html_e( 'Plugin', 'dt_launcher' ); ?></h1>
 </header>
 
-<div>
-	<?php echo $this->section( 'content' ) ?>
-</div>
+<div class="container">
+    <dt-tile>
+        <div class="section__inner">
+            <div>
+                <dt-copy-text value="<?php echo DT\Launcher\magic_url(); ?>"></dt-copy-text>
+            </div>
+            <div>
+                <?php echo $this->section( 'content' ) ?>
+            </div>
+        </div>
 
-<footer>
+    </dt-tile>
+
+</div>
+<footer class="footer">
     <p>
 		<?php $this->esc_html_e( 'Copyright ', 'dt_launcher' ); ?>
 
