0e5df0c559503b43ce0f087a3268ce6c95bb6d92
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-01 18:30:21 +0530

    app utility tab
    commite to app utility tab

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -1,9 +1,11 @@
 import "../css/plugin.css";
 import "@disciple.tools/web-components";
 import "./action-group.js";
-import '@spectrum-web-components/tabs/sp-tabs.js';
-import '@spectrum-web-components/tabs/sp-tab.js';
-import '@spectrum-web-components/tabs/sp-tab-panel.js';
+import '@spectrum-web-components/button/sp-button.js';
+import '@spectrum-web-components/button/sp-clear-button.js';
+import '@spectrum-web-components/button/sp-close-button.js';
+import "@spectrum-web-components/progress-circle/sp-progress-circle.js";
+import "./dt-launcher-footer.js";
 
 
 console.log('plugin.js')
--- resources/views/layouts/plugin.php
+++ resources/views/layouts/plugin.php
@@ -3,24 +3,9 @@
 </header>
 
 <div class="container">
-    <dt-tile>
-        <div class="section__inner">
-            <div>
-                <dt-copy-text value="<?php echo DT\Launcher\magic_url(); ?>"></dt-copy-text>
-            </div>
-            <div>
-                <?php echo $this->section( 'content' ) ?>
-            </div>
-        </div>
-
-        <div class="tab-container">
-            <sp-tabs  class="border-tab" selected="1" size="s">
-                <sp-tab label="Install as App" value="1"></sp-tab>
-                <sp-tab label="Hidden Apps" value="2"></sp-tab>
-            </sp-tabs>
-        </div>
-
-    </dt-tile>
+    <div>
+        <?php echo $this->section( 'content' ) ?>
+    </div>
 </div>
 
 <footer class="footer">
