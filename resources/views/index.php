c0702741f6bb9c2d0769a4f73c216cb54d4bad3e
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-02-05 10:42:31 +0530

    Update index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -7,22 +7,20 @@ $this->layout('layouts/plugin');
 
     <dt-tile>
         <div class="section__inner">
+
+            <dt-launcher-app-grid id="appGrid" app-data='<?php echo htmlspecialchars($data); ?>'>
+                <!-- Add more app icons as needed -->
+            </dt-launcher-app-grid>
             <div>
-                <dt-copy-text value="<?php echo esc_url( $magic_url ); ?>"></dt-copy-text>
-            </div>
-          <dt-launcher-app-grid id="appGrid" app-data='<?php echo htmlspecialchars($data); ?>'>
-    <!-- Add more app icons as needed -->
-</dt-launcher-app-grid>
-            <div>
-                <?php echo $this->section( 'content' ) ?>
+                <?php echo $this->section('content') ?>
             </div>
 
             <b>
-                Name: <?php echo $this->e( $user->user_nicename ); ?>
+                Name: <?php echo $this->e($user->user_nicename); ?>
             </b>
 
-            <a href="<?php echo esc_url( $subpage_url ); ?>">
-                <?php $this->esc_html_e( 'Visit subpage', 'dt-launcher' ); ?>
+            <a href="<?php echo esc_url($subpage_url); ?>">
+                <?php $this->esc_html_e('Visit subpage', 'dt-launcher'); ?>
             </a>
 
             <dt-launcher-footer></dt-launcher-footer>
