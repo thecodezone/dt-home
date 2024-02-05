0ac2055c727639cbac9646f9fcd9cbfbd838b395
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-02-05 16:04:27 +0530

    Revert "Moved the menu within the lit component."
    This reverts commit 45e0fd560eaab28e749fb9910728442b2a22ff46.

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -1,4 +1,3 @@
-import {css, html, LitElement} from 'lit';
 import '@spectrum-web-components/menu/sp-menu.js';
 import '@spectrum-web-components/menu/sp-menu-group.js';
 import '@spectrum-web-components/menu/sp-menu-item.js';
@@ -7,118 +6,51 @@ import '@spectrum-web-components/overlay/sp-overlay.js';
 import '@spectrum-web-components/icon/sp-icon.js';
 import '@spectrum-web-components/icons-ui/icons/sp-icon-triple-gripper.js';
 
-class MenuComponent extends LitElement {
-  static properties = {
-    isOpen: {type: Boolean}
-  };
-  static styles = css`
-    .menu-button {
-      float: inline-end;
-    }
+$('#trigger').click(function () {
 
-    .inline-element {
-      display: inline-block;
-      vertical-align: top;
-    }
+  var $button = $(this);
+  var $overlay = $button.next('sp-overlay');
+  var $popover = $overlay.find('sp-popover');
 
-    sp-icon-triple-gripper {
-      border: 2px solid #1658a9;
-      border-radius: 50%;
-      padding: 5px;
-      display: inline-block;
-    }
-
-    sp-popover {
-      background-color: #ffff;
-      min-width: 240%;
-      margin-left: -202%;
-      border: 2px solid #7a76767d;
-      padding: 10px;
-    }
-
-    .right-aligned-menu sp-menu-item {
-      padding: 5px 0px;
-      font-weight: 100;
-    }
-
-    .menu-title {
-      font-weight: 100;
-    }
-
-    @media (min-width: 230px) and (max-width: 950px) {
-      sp-popover {
-        background-color: #ffff;
-        min-width: 175%;
-        margin-left: -94%;
-        border: 2px solid #7a76767d;
-        padding: 10px;
-      }
-    }
-
-    @media (min-width: 750px) and (max-width: 950px) {
-      sp-popover {
-        background-color: #ffff;
-        min-width: 175%;
-        margin-left: -152%;
-        border: 2px solid #7a76767d;
-        padding: 10px;
-      }
-    }
-
-    .right-aligned-menu {
-      text-align: left;
-    }
-
-
-    .toggle-button {
-      background-color: #f0f0f0;
-      border-radius: 19px;
-      display: flex;
-    }
-
-    .toggle-button.active {
-      background-color: #0782eb;
-      border-radius: 19px;
-      display: flex;
-    }
-  `;
-
-  constructor() {
-    super();
-    this.isOpen = false;
+  if ($popover.css('position') === 'relative') {
+    $popover.css('position', '');
+    $button.css({
+      'background-color': '',
+      'border-radius': '',
+      'padding': '',
+      'display': ''
+    });
+  } else {
+    $popover.css('position', 'relative');
+    $button.css({
+      'background-color': '#007bff',
+      'border-radius': '45%',
+      'padding': '0px',
+      'display': 'flex'
+    });
   }
+});
 
-  togglePopover() {
-    this.isOpen = !this.isOpen;
+$(document).click(function (event) {
+
+  var $overlay = $('sp-overlay');
+  var $button = $('#trigger');
+  var $popover = $overlay.find('sp-popover');
+
+  if (!$overlay.is(event.target) && !$overlay.has(event.target).length && !$button.is(event.target) && !$button.has(event.target).length && $popover.css('position') === 'relative') {
+    $popover.css('position', '');
+    $button.css('background-color', '');
   }
+});
 
-  render() {
-    return html`
-      <sp-button
-        id="trigger"
-        placement="right"
-        class="menu-button inline-element toggle-button ${this.isOpen ? 'active' : ''}"
-        @click="${this.togglePopover}"
-      >
-        <sp-icon-triple-gripper slot="icon"></sp-icon-triple-gripper>
-      </sp-button>
 
-      ${this.isOpen ? html`
-        <sp-overlay open trigger="trigger@click" placement="bottom" style="position: relative">
-          <sp-popover .open="${this.isOpen}">
-            <sp-dialog>
-              <h4 slot="heading" class="menu-title">Go to disciple.tools</h4>
-              <sp-menu class="right-aligned-menu">
-                <sp-menu-item>Training</sp-menu-item>
-                <sp-menu-item>Log Out</sp-menu-item>
-              </sp-menu>
-            </sp-dialog>
-          </sp-popover>
-        </sp-overlay>
-      ` : ''}
-    `;
-  }
 
-}
 
-customElements.define('menu-component', MenuComponent);
+
+
+
+
+
+
+
+
