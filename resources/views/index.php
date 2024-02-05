a33aed96a89171fda5a41fe8197b41877ae93c65
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-02-05 16:04:03 +0530

    Revert "Revert "Moved the menu within the lit component.""
    This reverts commit 9fd29675fdd52c32ef1a2061ab007f03aba5c029.

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,45 +4,18 @@ $this->layout('layouts/plugin');
 
 <div class="container">
 
-    <dt-tile>
-        <div class="section__inner">
+    <div>
+        <?php echo $this->section('content') ?>
+    </div>
 
-            <sp-button id="trigger" placement="right" class="menu-button inline-element" style>
-                <sp-icon-triple-gripper slot="icon"></sp-icon-triple-gripper>
-            </sp-button>
-            <sp-overlay trigger="trigger@click" placement="bottom">
-                <sp-popover open style="position: relative">
-                    <sp-dialog>
-                        <?php if ($user->has_cap('access_disciple_tools')): ?>
-                            <h4 slot="heading" class="menu-title"> <?php esc_html_e('Go to disciple.tools'); ?></h4>
-                            <sp-menu class="right-aligned-menu">
-                                <sp-menu-item>
-                                    <?php esc_html_e('Training'); ?>
-                                </sp-menu-item>
-                                <sp-menu-item>
-                                    <?php esc_html_e('Log Out'); ?>
+    <b>
+        Name: <?php echo $this->e($user->user_nicename); ?>
+    </b>
 
-                                </sp-menu-item>
-                            </sp-menu>
-                        <?php endif; ?>
-                    </sp-dialog>
-                </sp-popover>
-            </sp-overlay>
-
-            <div>
-                <?php echo $this->section('content') ?>
-            </div>
-
-            <b>
-                Name: <?php echo $this->e($user->user_nicename); ?>
-            </b>
-
-            <a href="<?php echo esc_url($subpage_url); ?>">
-                <?php $this->esc_html_e('Visit subpage', 'dt-launcher'); ?>
-            </a>
+    <a href="<?php echo esc_url($subpage_url); ?>">
+        <?php $this->esc_html_e('Visit subpage', 'dt-launcher'); ?>
+    </a>
 
 
-        </div>
-    </dt-tile>
 </div>
 
--- resources/views/layouts/plugin.php
+++ resources/views/layouts/plugin.php
@@ -1,17 +1,24 @@
 <header>
-    <h1><?php $this->esc_html_e( 'Plugin', 'dt_launcher' ); ?></h1>
+    <h1><?php $this->esc_html_e('Plugin', 'dt_launcher'); ?></h1>
 </header>
 
 <div class="container">
-    <div>
-        <?php echo $this->section( 'content' ) ?>
-    </div>
+    <dt-tile>
+        <div class="section__inner">
+            <?php $user = wp_get_current_user();
+            ?>
+            <?php if ($user->has_cap('access_disciple_tools')): ?>
+                <menu-component></menu-component>
+            <?php endif; ?>
+            <?php echo $this->section('content') ?>
+
+            <footer class="footer">
+                <p>
+                    <?php $this->esc_html_e('Copyright ', 'dt_launcher'); ?>
+
+                    <?php echo $this->e(gmdate('Y')); ?>
+                </p>
+            </footer>
+        </div>
 </div>
-
-<footer class="footer">
-    <p>
-		<?php $this->esc_html_e( 'Copyright ', 'dt_launcher' ); ?>
-
-		<?php echo $this->e( gmdate( 'Y' ) ); ?>
-    </p>
-</footer>
+</dt-tile>
