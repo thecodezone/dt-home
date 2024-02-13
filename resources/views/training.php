34328b2475732c3fd328d4e656b6495bacbcd7d7
Craig Wann <craig@codezone.io>
2024-02-13 16:17:31 -0600

    Update training.php

--- resources/views/training.php
+++ resources/views/training.php
@@ -3,7 +3,7 @@ $this->layout( 'layouts/plugin' );
 ?>
 
 <div>
-    <?php $this->esc_html_e('Training', 'dt-launcher'); ?>
+    <?php $this->esc_html_e('Training', 'dt_home'); ?>
     <br><br><br>
     <video-list training-data='<?php echo htmlspecialchars($data); ?>'></video-list>
 
