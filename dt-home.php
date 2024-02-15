699262fadf8edc7f8fc320f3e220229bd19ba197
Craig Wann <craig@codezone.io>
2024-02-15 12:48:09 -0600

    Removing extra slash from vendor includes.

--- dt-home.php
+++ dt-home.php
@@ -25,9 +25,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-require_once plugin_dir_path( __FILE__ ) . '/vendor-scoped/scoper-autoload.php';
-require_once plugin_dir_path( __FILE__ ) . '/vendor-scoped/autoload.php';
-require_once plugin_dir_path( __FILE__ ) . '/vendor/autoload.php';
+require_once plugin_dir_path( __FILE__ ) . 'vendor-scoped/scoper-autoload.php';
+require_once plugin_dir_path( __FILE__ ) . 'vendor-scoped/autoload.php';
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
 $container = new Container();
 $container->singleton( Container::class, function ( $container ) {
