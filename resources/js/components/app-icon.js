132470e2c573acd0a2d56bbcb18811cfab34b2ce
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-22 10:19:53 +0530

    hidden app menu
    commite to hidden app menu

--- resources/js/components/app-icon.js
+++ resources/js/components/app-icon.js
@@ -6,94 +6,91 @@ import {css, html, LitElement} from 'lit';
  * @extends LitElement
  */
 class AppIcon extends LitElement {
-    static properties = {
-        name: {type: String},
-        icon: {type: String},
-        isVisible: {type: Boolean}
-    };
+  static properties = {
+    name: {type: String},
+    icon: {type: String},
+    isVisible: {type: Boolean}
+  };
 
-    /**
-     * CSS styles for the app icon.
-     * @typedef {String} appIconContainerStyle
-     */
-    static styles = css`
-        .app-icon__container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            width: 100%;
-        }
-
-        .app-icon__icon {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            aspect-ratio: auto 60 / 60;
-            background-color: #f0f0f0;
-            border-radius: 20px;
-            width: 100%;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-            margin-bottom: 8px;
-            cursor: pointer;
-            transition: transform 0.3s ease;
-            pointer-events: none;
-        }
-
-        .app-icon__icon:hover {
-            transform: scale(1.05);
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-        }
-
-        .app-icon__icon img {
-            width: 40%;
-        }
-
-        .app-icon__name {
-            font-size: 14px;
-            color: #333;
-            text-align: center;
-            word-wrap: break-word;
-            width: 80px;
-        }
-    `;
-
-    /**
-     * Creates a new instance of the constructor.
-     *
-     * @constructor
-     * @classdesc This constructor initializes a new object with the following properties:
-     *      - name: A string representing the name of the object.
-     *      - icon: A string representing the icon of the object.
-     *      - isVisible: A boolean indicating the visibility of the object.
-     *
-     * @returns {void} This constructor does not return a value.
-     */
-    constructor() {
-        super();
-        this.name = '';
-        this.icon = '';
-        this.isVisible = true;
+  /**
+   * CSS styles for the app icon.
+   * @typedef {String} appIconContainerStyle
+   */
+  static styles = css`
+    .app-icon__container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
     }
 
-
-    /**
-     * Renders the app icon.
-     * @returns {html} - The rendered HTML for the app icon.
-     */
-    render() {
-        return this.isVisible ? html`
-            <div class="app-icon__container">
-                <div class="app-icon__icon">
-                    <img src="${this.icon}"/>
-                </div>
-                <span class="app-icon__name">${this.name}</span>
-
-            </div>
-        ` : html``;
+    .app-icon__icon {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      aspect-ratio: auto 60 / 60;
+      background-color: #f0f0f0;
+      border-radius: 20px;
+      width: 100%;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+      margin-bottom: 8px;
+      cursor: pointer;
+      transition: transform 0.3s ease;
+      pointer-events: none;
     }
+
+    .app-icon__icon:hover {
+      transform: scale(1.05);
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+    }
+
+    .app-icon__icon img {
+      width: 40%;
+    }
+
+    .app-icon__name {
+      font-size: 14px;
+      color: #333;
+      text-align: center;
+      word-wrap: break-word;
+      width: 80px;
+    }
+  `;
+
+  /**
+   * Creates a new instance of the constructor.
+   *
+   * @constructor
+   * @classdesc This constructor initializes a new object with the following properties:
+   *      - name: A string representing the name of the object.
+   *      - icon: A string representing the icon of the object.
+   *      - isVisible: A boolean indicating the visibility of the object.
+   *
+   * @returns {void} This constructor does not return a value.
+   */
+  constructor() {
+    super();
+    this.name = '';
+    this.icon = '';
+    this.isVisible = true;
+  }
+
+
+  /**
+   * Renders the app icon.
+   * @returns {html} - The rendered HTML for the app icon.
+   */
+  render() {
+    return this.isVisible ? html`
+      <div class="app-icon__container">
+        <div class="app-icon__icon">
+          <img src="${this.icon}"/>
+        </div>
+        <span class="app-icon__name">${this.name}</span>
+
+      </div>
+    ` : html``;
+  }
 }
 
 customElements.define('dt-home-app-icon', AppIcon);
-
-
-
--- src/Controllers/MagicLink/HomeController.php
+++ src/Controllers/MagicLink/HomeController.php
@@ -8,119 +8,130 @@ use function DT\Home\magic_url;
 use function DT\Home\template;
 
 
-class HomeController {
-	public function show( Request $request, Response $response, $key ) {
-		$user        = wp_get_current_user();
-		$subpage_url = magic_url( 'subpage', $key );
+class HomeController
+{
+    public function show(Request $request, Response $response, $key)
+    {
+        $user = wp_get_current_user();
+        $subpage_url = magic_url('subpage', $key);
 
-		$apps_array = get_option( 'dt_home_apps', [] );
-		$data       = json_encode( $apps_array );
-		$app_url    = magic_url( '', $key );
-		$magic_link = magic_url();
+        $apps_array = get_option('dt_home_apps', []);
+        $hidden_apps_array = get_option('dt_home_apps', []);
 
-		return template( 'index', compact(
-			'user',
-			'subpage_url',
-			'data',
-			'app_url',
-			'magic_link'
-		) );
-	}
+        $data = json_encode($apps_array);
+        $hidden_data = json_encode($hidden_apps_array);
+        $app_url = magic_url('', $key);
+        $magic_link = magic_url();
 
-	public function show_hidden_apps( Request $request, Response $response, $key ) {
-		$user        = wp_get_current_user();
-		$subpage_url = magic_url( 'subpage', $key );
-		$magic_link  = magic_url();
+        return template('index', compact(
+            'user',
+            'subpage_url',
+            'data',
+            'app_url',
+            'magic_link',
+            'hidden_data'
+        ));
+    }
 
-		$apps_array = get_option( 'dt_home_apps', [] );
-		$data       = json_encode( $apps_array );
-		$app_url    = magic_url( '', $key );
+    public function show_hidden_apps(Request $request, Response $response, $key)
+    {
+        $user = wp_get_current_user();
+        $subpage_url = magic_url('subpage', $key);
+        $magic_link = magic_url();
 
-		return template( 'hidden-apps', compact(
-			'user',
-			'subpage_url',
-			'data',
-			'app_url',
-			'magic_link'
-		) );
-	}
+        $apps_array = get_option('dt_home_apps', []);
+        $data = json_encode($apps_array);
+        $app_url = magic_url('', $key);
 
-	public function data( Request $request, Response $response, $key ) {
-		$user = wp_get_current_user();
-		$data = [
-			'user_login' => $user->user_login,
-		];
-		$response->setContent( $data );
+        return template('hidden-apps', compact(
+            'user',
+            'subpage_url',
+            'data',
+            'app_url',
+            'magic_link'
+        ));
+    }
 
-		return $response;
-	}
+    public function data(Request $request, Response $response, $key)
+    {
+        $user = wp_get_current_user();
+        $data = [
+            'user_login' => $user->user_login,
+        ];
+        $response->setContent($data);
 
-	public function update_hide_app( Request $request, Response $response, $key ) {
-		$data = $request->json()->all();
+        return $response;
+    }
 
-		// Assuming $data contains 'id' and 'is_hidden'
-		$app_id = $data['id'];
+    public function update_hide_app(Request $request, Response $response, $key)
+    {
+        $data = $request->json()->all();
 
-		$apps_array = get_option( 'dt_home_apps', [] );
+        // Assuming $data contains 'id' and 'is_hidden'
+        $app_id = $data['id'];
 
-		// Find the app with the specified ID and update its 'is_hidden' status
-		foreach ( $apps_array as $key => $app ) {
-			if ( isset( $app['id'] ) && $app['id'] == $app_id ) {
-				$apps_array[ $key ]['is_hidden'] = 1; // Set 'is_hidden' to 1 (hide)
-				break; // Exit the loop once the app is found and updated
-			}
-		}
-		// Save the updated array back to the option
-		update_option( 'dt_home_apps', $apps_array );
+        $apps_array = get_option('dt_home_apps', []);
 
-		$response_data = [ 'message' => 'App visibility updated' ];
+        // Find the app with the specified ID and update its 'is_hidden' status
+        foreach ($apps_array as $key => $app) {
+            if (isset($app['id']) && $app['id'] == $app_id) {
+                $apps_array[$key]['is_hidden'] = 1; // Set 'is_hidden' to 1 (hide)
+                break; // Exit the loop once the app is found and updated
+            }
+        }
+        // Save the updated array back to the option
+        update_option('dt_home_apps', $apps_array);
 
-		$response->setContent( json_encode( $response_data ) );
+        $response_data = ['message' => 'App visibility updated'];
 
-		return $response;
-	}
+        $response->setContent(json_encode($response_data));
 
-	public function update_unhide_app( Request $request, Response $response, $key ) {
-		$data = $request->json()->all();
+        return $response;
+    }
 
-		// Assuming $data contains 'id' and 'is_hidden'
-		$app_id = $data['id'];
+    public function update_un_hide_app(Request $request, Response $response)
+    {
+        $data = json_decode($request->getContent(), true); // Get the JSON payload and decode it
+        $app_id = $data['id'] ?? null; // Use the null coalescing operator to set a default
 
-		$apps_array = get_option( 'dt_home_apps', [] );
+        if ($app_id !== null) {
+            $apps_array = get_option('dt_home_apps', []);
 
-		// Find the app with the specified ID and update its 'is_hidden' status
-		foreach ( $apps_array as $key => $app ) {
-			if ( isset( $app['id'] ) && $app['id'] == $app_id ) {
-				$apps_array[ $key ]['is_hidden'] = 0; // Set 'is_hidden' to 1 (hide)
-				break; // Exit the loop once the app is found and updated
-			}
-		}
-		// Save the updated array back to the option
-		update_option( 'dt_home_apps', $apps_array );
+            foreach ($apps_array as $key => $app) {
+                if (isset($app['id']) && $app['id'] == $app_id) {
+                    $apps_array[$key]['is_hidden'] = '0'; // Corrected: Set 'is_hidden' to 0 (unhide)
+                    break;
+                }
+            }
 
-		$response_data = [ 'message' => 'App visibility updated' ];
+            update_option('dt_home_apps', $apps_array);
 
-		$response->setContent( json_encode( $response_data ) );
+            return response()->json(['message' => 'App visibility updated'], 200);
+        } else {
+            return response()->json(['error' => 'Invalid app ID'], 400);
+        }
+    }
 
-		return $response;
-	}
 
-	public function update_app_order( Request $request, Response $response, $key ): Response {
-		$data = $request->json()->all();
-		// Iterate through each app in the data
-		foreach ( $data as $key => $app ) {
-			// Update the 'sort' field for each app based on its position in the array
-			$data[ $key ]['sort'] = $key + 1;
-		}
-		// Save the updated app order back to the database or storage
-		update_option( 'dt_home_apps', $data );
+    public function update_app_order(Request $request, Response $response, $key): Response
+    {
+        $data = $request->json()->all();
+        // Iterate through each app in the data
+        foreach ($data as $key => $app) {
+            // Update the 'sort' field for each app based on its position in the array
+            $data[$key]['sort'] = $key + 1;
+        }
+        // Save the updated app order back to the database or storage
+        update_option('dt_home_apps', $data);
 
-		$response_data = [ 'message' => 'App order updated' ];
+        $response_data = ['message' => 'App order updated'];
 
-		$response->headers->set( 'Content-Type', 'application/json' );
+        $response->headers->set('Content-Type', 'application/json');
 
-		$response->setContent( json_encode( $response_data ) );
+        $response->setContent(json_encode($response_data));
 
-		return $response;
-	}
+        return $response;
+    }
+
+
 }
