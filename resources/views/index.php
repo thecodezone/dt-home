4c2fc43f5036644dee125c67f00cdd0f47ea122a
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-22 09:34:54 +0530

    hidden app menu
    commite to hidden app menu

--- resources/views/index.php
+++ resources/views/index.php
@@ -25,15 +25,4 @@ $this->layout('layouts/plugin');
     <dt-home-footer id="hiddenApps"
                     hidden-data='<?php echo esc_attr(htmlspecialchars($hidden_data)); ?>'
                     app-url-unhide='<?php echo esc_url($app_url); ?>'></dt-home-footer>
-    <script>
-        document.addEventListener('app-selected', (event) => {
-            const appUrl = event.detail.url;
-            // Logic to extract a slug or identifier from the URL
-            const appSlug = extractSlugFromUrl(appUrl); // Implement this function based on your URL structure
-
-            // Navigate to the new route
-            // This will depend on your routing setup, for example:
-            // window.location.href = `/home/app/${encodeURIComponent(appSlug)}`;
-        });
-    </script>
 </div>
