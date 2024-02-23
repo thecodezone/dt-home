fe397fb0fc5bc31a6ae03d9bc0733b294eda510b
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-23 15:41:14 +0530

    hidden app menu 2
    commit to hidden app menu 2

--- src/Controllers/MagicLink/HomeController.php
+++ src/Controllers/MagicLink/HomeController.php
@@ -10,16 +10,16 @@ use function DT\Home\template;
 
 class HomeController
 {
-    public function show(Request $request, Response $response, $key)
+    public function show( Request $request, Response $response, $key )
     {
         $user = wp_get_current_user();
-        $subpage_url = magic_url('subpage', $key);
+        $subpage_url = magic_url( 'subpage', $key );
 
-        $apps_array = get_option('dt_home_apps', []);
+        $apps_array = get_option( 'dt_home_apps', [] );
 
-        $data = json_encode($apps_array);
-        $hidden_data = json_encode($apps_array);
-        $app_url = magic_url('', $key);
+        $data = json_encode( $apps_array );
+        $hidden_data = json_encode( $apps_array );
+        $app_url = magic_url( '', $key );
         $magic_link = magic_url();
 
         return template('index', compact(
@@ -33,17 +33,17 @@ class HomeController
     }
 
 
-    public function show_hidden_apps(Request $request, Response $response, $key)
+    public function show_hidden_apps( Request $request, Response $response, $key )
     {
         $user = wp_get_current_user();
-        $subpage_url = magic_url('subpage', $key);
+        $subpage_url = magic_url( 'subpage', $key );
         $magic_link = magic_url();
 
-        $apps_array = get_option('dt_home_apps', []);
-        $data = json_encode($apps_array);
+        $apps_array = get_option( 'dt_home_apps', [] );
+        $data = json_encode( $apps_array );
 
 
-        $app_url = magic_url('', $key);
+        $app_url = magic_url( '', $key );
 
         return template('hidden-apps', compact(
             'user',
@@ -51,89 +51,88 @@ class HomeController
             'data',
             'app_url',
             'magic_link',
-
         ));
     }
 
-    public function data(Request $request, Response $response, $key)
+    public function data( Request $request, Response $response, $key )
     {
         $user = wp_get_current_user();
         $data = [
             'user_login' => $user->user_login,
         ];
-        $response->setContent($data);
+        $response->setContent( $data );
 
         return $response;
     }
 
-    public function update_hide_app(Request $request, Response $response, $key)
+    public function update_hide_app( Request $request, Response $response, $key )
     {
         $data = $request->json()->all();
 
         // Assuming $data contains 'id' and 'is_hidden'
         $app_id = $data['id'];
 
-        $apps_array = get_option('dt_home_apps', []);
+        $apps_array = get_option( 'dt_home_apps', [] );
 
         // Find the app with the specified ID and update its 'is_hidden' status
-        foreach ($apps_array as $key => $app) {
-            if (isset($app['id']) && $app['id'] == $app_id) {
+        foreach ( $apps_array as $key => $app ) {
+            if ( isset( $app['id'] ) && $app['id'] == $app_id ) {
                 $apps_array[$key]['is_hidden'] = 1; // Set 'is_hidden' to 1 (hide)
                 break; // Exit the loop once the app is found and updated
             }
         }
         // Save the updated array back to the option
-        update_option('dt_home_apps', $apps_array);
+        update_option( 'dt_home_apps', $apps_array );
 
-        $response_data = ['message' => 'App visibility updated'];
+        $response_data = [ 'message' => 'App visibility updated' ];
 
-        $response->setContent(json_encode($response_data));
+        $response->setContent( json_encode( $response_data ) );
 
         return $response;
     }
 
-    public function update_unhide_app(Request $request, Response $response, $key)
+    public function update_unhide_app( Request $request, Response $response, $key )
     {
         $data = $request->json()->all();
 
         // Assuming $data contains 'id' and 'is_hidden'
         $app_id = $data['id'];
 
-        $apps_array = get_option('dt_home_apps', []);
+        $apps_array = get_option( 'dt_home_apps', [] );
 
         // Find the app with the specified ID and update its 'is_hidden' status
-        foreach ($apps_array as $key => $app) {
-            if (isset($app['id']) && $app['id'] == $app_id) {
+        foreach ( $apps_array as $key => $app ) {
+            if ( isset( $app['id'] ) && $app['id'] == $app_id ) {
                 $apps_array[$key]['is_hidden'] = 0; // Set 'is_hidden' to 1 (hide)
                 break; // Exit the loop once the app is found and updated
             }
         }
         // Save the updated array back to the option
-        update_option('dt_home_apps', $apps_array);
+        update_option( 'dt_home_apps', $apps_array );
 
-        $response_data = ['message' => 'App visibility updated'];
+        $response_data = [ 'message' => 'App visibility updated' ];
 
-        $response->setContent(json_encode($response_data));
+        $response->setContent( json_encode( $response_data ) );
 
         return $response;
     }
 
-    public function update_app_order(Request $request, Response $response, $key): Response
+    public function update_app_order( Request $request, Response $response, $key ): Response
     {
         $data = $request->json()->all();
         // Iterate through each app in the data
-        foreach ($data as $key => $app) {
+        foreach ( $data as $key => $app ) {
             // Update the 'sort' field for each app based on its position in the array
             $data[$key]['sort'] = $key + 1;
         }
         // Save the updated app order back to the database or storage
-        update_option('dt_home_apps', $data);
+        update_option( 'dt_home_apps', $data );
 
-        $response_data = ['message' => 'App order updated'];
+        $response_data = [ 'message' => 'App order updated' ];
 
-        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set( 'Content-Type', 'application/json' );
 
-        $response->setContent(json_encode($response_data));
+        $response->setContent( json_encode( $response_data ) );
 
         return $response;
     }
