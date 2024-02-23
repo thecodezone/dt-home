8188fd0a1c7e9fd033e4a3e2c9ea064e8f84e530
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-23 12:43:29 +0530

    load app in iframe
    commite to load app in iframe

---
+++ resources/views/web-view.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @var string $magic_link
+ * @var string $data
+ * @var string $app_url
+ * @var string $subpage_url
+ * @var WP_User $user
+ */
+$this->layout('layouts/plugin');
+?>
+
+<div>
+    <br><br><br>
+    <?php
+    if (isset($desired_app) && isset($desired_app['url'])) {
+        $url = $desired_app['url'];
+        $cleanedUrl = preg_replace('/\\\\+/', '', $url);
+        ?>
+        <iframe src="https://www.theknowledgeacademy.com/blog/demo-website-for-automation-testing/" width="400"
+                height="600"></iframe>
+
+
+        <!--       <iframe src="--><?php //echo htmlspecialchars($url); ?><!--" width="400" height="600" frameborder="0"></iframe>-->
+        <?php
+    } else {
+        echo "URL not found or the desired app is not set.";
+    }
+    ?>
+
+
+    <div>
+        <?php echo $this->section('content') ?>
+    </div>
+</div>
