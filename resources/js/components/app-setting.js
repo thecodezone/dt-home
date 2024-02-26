ec60f632cdedb5f234f0ce18a9c3f96ab55dfc58
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-26 10:31:23 +0530

    Update app-setting.js

--- resources/js/components/app-setting.js
+++ resources/js/components/app-setting.js
@@ -8,6 +8,19 @@ window.hidePopup = function () {
   document.getElementById("popup").style.display = "none";
 }
 
+/**
+ * Dynamically loads and displays SVG icons in a specified container element on the webpage.
+ * The SVG icon URLs are expected to be provided externally, typically set on the `window.svgIconUrls` variable.
+ * Each SVG icon is presented as an image element. When an icon is clicked, its URL is assigned to
+ * an input element with ID 'icon', and a popup (if any) is hidden.
+ *
+ * This function is designed to be called when there is a need to populate a container with SVG icons,
+ * for instance, after receiving SVG URL data from a server.
+ *
+ * @memberof YourModuleNameOrComponentName  // Replace with the actual module or component name
+ * @function loadSVGIcons
+ * @returns {void}
+ */
 function loadSVGIcons() {
   var container = document.getElementById("svgContainer");
   container.innerHTML = '';
