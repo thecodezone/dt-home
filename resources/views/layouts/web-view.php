433f369204f46d737c48f4be948cdaf3ed2fd862
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-26 11:15:04 +0530

    load app in iframe
    commite to load app in iframe

---
+++ resources/views/layouts/web-view.php
@@ -0,0 +1,12 @@
+<div class="cloak">
+    <div class="container">
+        <dt-tile>
+            <div class="section__inner">
+                <?php $user = wp_get_current_user();
+                ?>
+                <?php echo $this->section('content') ?>
+            </div>
+        </dt-tile>
+    </div>
+</div>
+
