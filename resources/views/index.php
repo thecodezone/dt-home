0719c2065dd7722f47f255129662086268f42043
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-02-26 20:39:06 +0530

    copy text provided

--- resources/views/index.php
+++ resources/views/index.php
@@ -22,7 +22,7 @@ $this->layout( 'layouts/plugin' );
         <sp-tooltip slot="hover-content" open placement="right" class="spl-text">
             <!-- Dynamic content goes here -->
             <?php
-            $text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book";
+            $text = "Copy this link and share it with people you are coaching.";
             $escaped_text = esc_attr( $text ); // If you need to ensure the text is safe for HTML attributes
             $newtext = wordwrap( $escaped_text, 35, "<br />\n" );
             echo $newtext;
