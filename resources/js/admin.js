7b4a152f82dce78a6fd70489567b2b9a641af12a
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-02-26 10:20:28 +0530

    edit create improvement
    commite to edit create improvement

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -2,4 +2,3 @@ import "../css/admin.css";
 import "@disciple.tools/web-components";
 import "./components/app-setting.js"
 
-console.log('admin js loaded')
--- src/Services/SVGIconService.php
+++ src/Services/SVGIconService.php
@@ -6,18 +6,18 @@ class SVGIconService
 {
     private $svg_dir_path;
 
-    public function __construct( $svg_dir_path )
+    public function __construct($svg_dir_path)
     {
         $this->svg_dir_path = $svg_dir_path;
     }
 
-    public function get_s_v_g_icon_u_r_ls()
+    public function get_svg_icon_urls()
     {
-        if ( is_dir( $this->svg_dir_path ) ) {
-            $svg_files = array_diff( scandir( $this->svg_dir_path ), [ '..', '.' ] );
+        if (is_dir($this->svg_dir_path)) {
+            $svg_files = array_diff(scandir($this->svg_dir_path), ['..', '.']);
 
-            return array_values(array_filter(array_map(function ( $file ) {
-                if ( pathinfo( $this->svg_dir_path . $file, PATHINFO_EXTENSION ) === 'svg' ) {
+            return array_values(array_filter(array_map(function ($file) {
+                if (pathinfo($this->svg_dir_path . $file, PATHINFO_EXTENSION) === 'svg') {
                     return get_template_directory_uri() . '/dt-assets/images/' . $file;
                 }
                 return false;
