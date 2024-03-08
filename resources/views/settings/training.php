271a19c24041475b251c63e47da38860603efaba
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-03-08 11:29:41 +0530

    Add space between pipe and link

--- resources/views/settings/training.php
+++ resources/views/settings/training.php
@@ -3,7 +3,7 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
 ?>
 
 <form method="post">
-	<?php wp_nonce_field( 'dt_admin_form', 'dt_admin_form_nonce' ) ?>
+    <?php wp_nonce_field( 'dt_admin_form', 'dt_admin_form_nonce' ) ?>
 
     <!-- Add a form -->
 </form>
@@ -14,40 +14,39 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
             <div class="col-md-12">
                     <span style="float:right;">
                         <a href="admin.php?page=dt_home&tab=training&action=create" class="button float-right"><i
-                                    class="fa fa-plus"></i> Add Training</a>
+                                class="fa fa-plus"></i> <?php echo esc_html( 'Add Training' ); ?></a>
                     </span>
 
                 <br><br>
 
-                <table class="widefat striped" style="border-collapse: collapse; width: 100%;">
+                <table class="widefat striped" style="border-collapse: collapse; width: 110%;">
                     <thead>
                     <tr>
-                        <th style="border: 1px solid #ddd;">Name</th>
-                        <th style="border: 1px solid #ddd;">Embed Code</th>
-                        <th style="border: 1px solid #ddd;">Anchor</th>
-                        <th style="border: 1px solid #ddd;">Action</th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Name' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Embed Code' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Anchor' ); ?></th>
+                        <th style="border: 1px solid #ddd;min-width: 100%"><?php echo esc_html( 'Action' ); ?></th>
                     </tr>
                     </thead>
                     <tbody>
-					<?php foreach ( $data as $training ) : ?>
+                    <?php foreach ( $data as $training ) : ?>
                         <tr>
                             <td style="border: 1px solid #ddd;"><?php echo esc_html( $training['name'] ); ?></td>
                             <td style="border: 1px solid #ddd;"><?php echo stripslashes( $training['embed_video'] ); ?></td>
                             <td style="border: 1px solid #ddd;">
-								<?php echo esc_html( $training['anchor'] ); ?>
+                                <?php echo esc_html( $training['anchor'] ); ?>
                             </td>
 
                             <td style="border: 1px solid #ddd;">
-                                <a href="admin.php?page=dt_home&tab=training&action=up/<?php echo esc_attr( $training['id'] ); ?>">Up</a>|
-                                &nbsp;
-                                <a href="admin.php?page=dt_home&tab=training&action=edit/<?php echo esc_attr( $training['id'] ); ?>">Edit</a>|&nbsp;
-                                <a href="#"
-                                   onclick="confirmDelete(<?php echo esc_attr( $training['id'] ); ?>)">Delete</a>|&nbsp;
-                                <a href="admin.php?page=dt_home&tab=training&action=down/<?php echo esc_attr( $training['id'] ); ?>">Down</a>
+                                <a href="admin.php?page=dt_home&tab=training&action=up/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html( 'Up' ); ?></a>&nbsp;|&nbsp;<a
+                                    href="admin.php?page=dt_home&tab=training&action=edit/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html( 'Edit' ); ?></a>&nbsp;|&nbsp;<a
+                                    href="#"
+                                    onclick="confirmDelete(<?php echo esc_attr( $training['id'] ); ?>)"><?php echo esc_html( 'Delete' ); ?></a>&nbsp;|&nbsp;<a
+                                    href="admin.php?page=dt_home&tab=training&action=down/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html( 'Down' ); ?></a>
                             </td>
 
                         </tr>
-					<?php endforeach; ?>
+                    <?php endforeach; ?>
                     </tbody>
                 </table>
 
