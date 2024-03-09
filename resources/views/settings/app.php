20a0210b281e9ad12a4cb27c86ba60618a74ed4e
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-03-09 09:49:49 +0530

    added 'esc_html_e()'

--- resources/views/settings/app.php
+++ resources/views/settings/app.php
@@ -14,7 +14,7 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
             <div class="col-md-12">
                     <span style="float:right;">
                         <a href="admin.php?page=dt_home&tab=app&action=create" class="button float-right"><i
-                                class="fa fa-plus"></i><?php echo esc_html( 'Add App' ); ?> </a>
+                                class="fa fa-plus"></i><?php echo esc_html_e( 'Add App' ); ?> </a>
                     </span>
 
                 <br><br>
@@ -22,17 +22,17 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
                 <table class="widefat striped" style="border-collapse: collapse; width: 100%;">
                     <thead>
                     <tr>
-                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Name' ); ?></th>
-                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Type' ); ?></th>
-                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Icon' ); ?></th>
-                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Action' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html_e( 'Name' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html_e( 'Type' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html_e( 'Icon' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html_e( 'Action' ); ?></th>
                     </tr>
                     </thead>
                     <tbody>
                     <?php foreach ( $data as $app ) : ?>
                         <tr>
-                            <td style="border: 1px solid #ddd;"><?php echo esc_html( $app['name'] ); ?></td>
-                            <td style="border: 1px solid #ddd;"><?php echo esc_html( $app['type'] ); ?></td>
+                            <td style="border: 1px solid #ddd;"><?php echo esc_html_e( $app['name'] ); ?></td>
+                            <td style="border: 1px solid #ddd;"><?php echo esc_html_e( $app['type'] ); ?></td>
                             <td style="border: 1px solid #ddd;">
                                 <?php if ( !empty( $app['icon'] ) ) : ?>
                                     <img src="<?php echo esc_url( $app['icon'] ); ?>" alt="Icon"
@@ -42,18 +42,18 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
 
                             <td style="border: 1px solid #ddd;">
 
-                                <a href="admin.php?page=dt_home&tab=app&action=up/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html( 'Up' ); ?></a>&nbsp;
+                                <a href="admin.php?page=dt_home&tab=app&action=up/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html_e( 'Up' ); ?></a>&nbsp;
                                 |&nbsp;
-                                <a href="admin.php?page=dt_home&tab=app&action=edit/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html( 'Edit' ); ?></a>&nbsp;
+                                <a href="admin.php?page=dt_home&tab=app&action=edit/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html_e( 'Edit' ); ?></a>&nbsp;
                                 |&nbsp;
 
                                 <?php if ( $app['is_hidden'] == 1 ) { ?>
-                                    <a href="admin.php?page=dt_home&tab=app&action=unhide/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html( 'Unhide' ); ?></a>&nbsp;|&nbsp;
+                                    <a href="admin.php?page=dt_home&tab=app&action=unhide/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html_e( 'Unhide' ); ?></a>&nbsp;|&nbsp;
                                 <?php } else { ?>
-                                    <a href="admin.php?page=dt_home&tab=app&action=hide/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html( 'Hide' ); ?></a>&nbsp;|&nbsp;
+                                    <a href="admin.php?page=dt_home&tab=app&action=hide/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html_e( 'Hide' ); ?></a>&nbsp;|&nbsp;
                                 <?php } ?>
 
-                                <a href="admin.php?page=dt_home&tab=app&action=down/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html( 'Down' ); ?></a>
+                                <a href="admin.php?page=dt_home&tab=app&action=down/<?php echo esc_attr( $app['id'] ); ?>"><?php echo esc_html_e( 'Down' ); ?></a>
                             </td>
 
                         </tr>
--- resources/views/settings/training.php
+++ resources/views/settings/training.php
@@ -14,7 +14,7 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
             <div class="col-md-12">
                     <span style="float:right;">
                         <a href="admin.php?page=dt_home&tab=training&action=create" class="button float-right"><i
-                                class="fa fa-plus"></i> <?php echo esc_html( 'Add Training' ); ?></a>
+                                class="fa fa-plus"></i> <?php echo esc_html_e( 'Add Training' ); ?></a>
                     </span>
 
                 <br><br>
@@ -22,27 +22,27 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
                 <table class="widefat striped" style="border-collapse: collapse; width: 110%;">
                     <thead>
                     <tr>
-                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Name' ); ?></th>
-                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Embed Code' ); ?></th>
-                        <th style="border: 1px solid #ddd;"><?php echo esc_html( 'Anchor' ); ?></th>
-                        <th style="border: 1px solid #ddd;min-width: 100%"><?php echo esc_html( 'Action' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html_e( 'Name' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html_e( 'Embed Code' ); ?></th>
+                        <th style="border: 1px solid #ddd;"><?php echo esc_html_e( 'Anchor' ); ?></th>
+                        <th style="border: 1px solid #ddd;min-width: 100%"><?php echo esc_html_e( 'Action' ); ?></th>
                     </tr>
                     </thead>
                     <tbody>
                     <?php foreach ( $data as $training ) : ?>
                         <tr>
-                            <td style="border: 1px solid #ddd;"><?php echo esc_html( $training['name'] ); ?></td>
+                            <td style="border: 1px solid #ddd;"><?php echo esc_html_e( $training['name'] ); ?></td>
                             <td style="border: 1px solid #ddd;"><?php echo stripslashes( $training['embed_video'] ); ?></td>
                             <td style="border: 1px solid #ddd;">
                                 <?php echo esc_html( $training['anchor'] ); ?>
                             </td>
 
                             <td style="border: 1px solid #ddd;">
-                                <a href="admin.php?page=dt_home&tab=training&action=up/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html( 'Up' ); ?></a>&nbsp;|&nbsp;<a
-                                    href="admin.php?page=dt_home&tab=training&action=edit/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html( 'Edit' ); ?></a>&nbsp;|&nbsp;<a
+                                <a href="admin.php?page=dt_home&tab=training&action=up/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html_e( 'Up' ); ?></a>&nbsp;|&nbsp;<a
+                                    href="admin.php?page=dt_home&tab=training&action=edit/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html_e( 'Edit' ); ?></a>&nbsp;|&nbsp;<a
                                     href="#"
-                                    onclick="confirmDelete(<?php echo esc_attr( $training['id'] ); ?>)"><?php echo esc_html( 'Delete' ); ?></a>&nbsp;|&nbsp;<a
-                                    href="admin.php?page=dt_home&tab=training&action=down/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html( 'Down' ); ?></a>
+                                    onclick="confirmDelete(<?php echo esc_attr( $training['id'] ); ?>)"><?php echo esc_html_e( 'Delete' ); ?></a>&nbsp;|&nbsp;<a
+                                    href="admin.php?page=dt_home&tab=training&action=down/<?php echo esc_attr( $training['id'] ); ?>"><?php echo esc_html_e( 'Down' ); ?></a>
                             </td>
 
                         </tr>
