ba5af142fc42867ef4e9274207ca86a8652502e7
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-03-11 14:02:46 +0530

    dt home screen icon
    commite to dt home screen icon

--- resources/views/web-view.php
+++ resources/views/web-view.php
@@ -3,11 +3,11 @@
  * @var array $app
  * @var string $url
  */
-$this->layout('layouts/web-view');
+$this->layout( 'layouts/web-view' );
 ?>
 
 <div class="app-container">
-    <iframe src="<?php echo htmlspecialchars($url); ?>" width="400" height="600" frameborder="0"></iframe>
+    <iframe src="<?php echo htmlspecialchars( $url ); ?>" width="400" height="600" frameborder="0"></iframe>
     <a href="/home" class="icon-link">
         <sp-icon-view-grid class="sp-icon-view-grid"></sp-icon-view-grid>
     </a>
