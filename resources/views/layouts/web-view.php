b4e1572f1f5929a6a4cafc27adcc0b8edd499efd
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-03-13 18:56:17 +0530

    iframe full screen
    commite to iframe full screen

--- resources/views/layouts/web-view.php
+++ resources/views/layouts/web-view.php
@@ -1,12 +1,6 @@
 <div class="cloak">
-    <div class="container">
-        <div>
-            <div class="section__inner">
-                <?php $user = wp_get_current_user();
-                ?>
-                <?php echo $this->section( 'content' ) ?>
-            </div>
-        </div>
-    </div>
+
+    <?php echo $this->section('content') ?>
+
 </div>
 
--- resources/views/web-view.php
+++ resources/views/web-view.php
@@ -3,12 +3,7 @@
  * @var array $app
  * @var string $url
  */
-$this->layout( 'layouts/web-view' );
+$this->layout('layouts/web-view');
 ?>
 
-<div class="app-container">
-    <iframe src="<?php echo htmlspecialchars( $url ); ?>" width="100%" height="650" frameborder="0"></iframe>
-    <a href="/home" class="icon-link">
-        <sp-icon-view-grid class="sp-icon-view-grid"></sp-icon-view-grid>
-    </a>
-</div>
+<iframe src="<?php echo htmlspecialchars($url); ?>" width="100%" height="650" frameborder="0"></iframe>
