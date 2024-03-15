f0c4d7b95b393f757a284f57b5a66b20d6d00137
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-03-15 10:30:47 +0530

    register background
    commite to register background

--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -1,58 +1,58 @@
 <?php
-$this->layout( 'layouts/auth' );
+$this->layout('layouts/auth');
 ?>
 <div class="container register">
-    <dt-tile>
+    <dt-tile class="register__background">
         <div class="section__inner">
             <div class="logo">
                 <img
-                        src="<?php echo esc_url( $logo_path ) ?>"
-                        alt="Disciple.Tools"
-                        class="logo__image">
+                    src="<?php echo esc_url($logo_path) ?>"
+                    alt="Disciple.Tools"
+                    class="logo__image">
             </div>
 
-            <form action="<?php echo esc_attr( $form_action ) ?>"
+            <form action="<?php echo esc_attr($form_action) ?>"
                   method="POST">
-				<?php wp_nonce_field( 'dt_autolink_register' ); ?>
+                <?php wp_nonce_field('dt_autolink_register'); ?>
 
-				<?php if ( ! empty( $error ) ) : ?>
+                <?php if (!empty($error)) : ?>
                     <dt-alert context="alert"
                               dismissable>
-						<?php echo esc_html( $error ) ?>
+                        <?php echo esc_html($error) ?>
                     </dt-alert>
-				<?php endif; ?>
+                <?php endif; ?>
 
                 <dt-text name="username"
-                         placeholder="<?php esc_attr_e( 'Username', 'disciple-tools-autolink' ); ?>"
-                         value="<?php echo esc_attr( $username ); ?>"
+                         placeholder="<?php esc_attr_e('Username', 'disciple-tools-autolink'); ?>"
+                         value="<?php echo esc_attr($username); ?>"
                          required></dt-text>
                 <dt-text name="email"
-                         placeholder="<?php esc_attr_e( 'Email', 'disciple-tools-autolink' ); ?>"
-                         value="<?php echo esc_attr( $email ); ?>"
+                         placeholder="<?php esc_attr_e('Email', 'disciple-tools-autolink'); ?>"
+                         value="<?php echo esc_attr($email); ?>"
                          required></dt-text>
                 <dt-text name="password"
-                         placeholder="<?php esc_attr_e( 'Password', 'disciple-tools-autolink' ); ?>"
-                         value="<?php echo esc_attr( $password ); ?>"
+                         placeholder="<?php esc_attr_e('Password', 'disciple-tools-autolink'); ?>"
+                         value="<?php echo esc_attr($password); ?>"
                          type="password"
                          required></dt-text>
                 <dt-text name="confirm_password"
-                         placeholder="<?php esc_attr_e( 'Confirm Password', 'disciple-tools-autolink' ); ?>"
+                         placeholder="<?php esc_attr_e('Confirm Password', 'disciple-tools-autolink'); ?>"
                          value=""
                          type="password"
                          required></dt-text>
 
-                <dt-button context="success"
-                           type="submit"
-                           class="button--large button--block">
-					<?php esc_html_e( 'Register', 'dt_home' ) ?>
-                </dt-button>
+                <sp-button class="register__button"
+                           tabindex="3"
+                           type="submit">
+                    <?php esc_html_e('Register', 'dt_home') ?>
+                </sp-button>
             </form>
         </div>
     </dt-tile>
     <div class="login__footer">
         <dt-button context="link"
-                   href="<?php echo esc_url( $login_url ); ?>">
-			<?php esc_html_e( 'Back to Login', 'dt_home' ); ?>
+                   href="<?php echo esc_url($login_url); ?>">
+            <?php esc_html_e('Back to Login', 'dt_home'); ?>
         </dt-button>
     </div>
 </div>
