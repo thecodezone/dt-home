253f5ef0fab539c7e194b57f42c146d1e5c9eeee
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-03-20 18:49:07 +0530

    wrap training videos
    commite the fixes into wrap training videos

--- resources/views/training.php
+++ resources/views/training.php
@@ -1,12 +1,12 @@
 <?php
 
-$this->layout('layouts/plugin');
+$this->layout( 'layouts/plugin' );
 ?>
-<h1 class="training"> <?php $this->esc_html_e('Training', 'dt_home'); ?></h1>
+<h1 class="training"> <?php $this->esc_html_e( 'Training', 'dt_home' ); ?></h1>
 <dt-tile>
 
 
-    <video-list training-data='<?php echo htmlspecialchars($data); ?>'></video-list>
+    <video-list training-data='<?php echo htmlspecialchars( $data ); ?>'></video-list>
 
 </dt-tile>
 
