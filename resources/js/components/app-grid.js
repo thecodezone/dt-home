ae37e4e7b97ef2e1d1ced4a13a36c22354bb1868
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-03-22 18:47:16 +0530

    Solve long press issue
    Double-clicking on an app icon after sorting resets the sort and Solve long press issue

--- resources/js/components/app-grid.js
+++ resources/js/components/app-grid.js
@@ -1,26 +1,28 @@
 import {css, html, LitElement} from 'lit';
-import Sortable from 'sortablejs';
-import "./app-icon.js";
 
 /**
- * Represents a grid of application icons.
- *
- * @extends LitElement
+ * Custom element representing an application grid.
  */
 class AppGrid extends LitElement {
-    static properties = {
-        appData: {type: Array},
-        selectedIndex: {type: Number},
-        appUrl: {type: String}
-
+  /**
+   * Properties of the AppGrid element.
+   * @static
+   * @type {Object}
+   */
+  static properties = {
+    appData: {type: Array},
+    selectedIndex: {type: Number},
+    appUrl: {type: String}
   };
 
   /**
-   * CSS styles for an app grid.
-   *
-   * @type {string}
+   * Styles for the AppGrid element.
+   * @static
+   * @type {CSSResult}
    */
   static styles = css`
+    /* CSS styles here */
+
     .app-grid {
       display: grid;
       grid-template-columns: repeat(3, 1fr);
@@ -53,134 +55,77 @@ class AppGrid extends LitElement {
       width: 100%;
       pointer-events: none;
     }
-
-    @media (min-width: 230px) and (max-width: 950px) {
-      .app-grid__remove-icon {
-        position: absolute;
-        top: -7px;
-        right: -10px;
-        background-color: rgb(255, 255, 255);
-        color: #fcfbfb;
-        padding: 5px 5px 0px 5px;
-        cursor: pointer;
-        border-radius: 53%;
-        font-size: 14px;
-        z-index: 1;
-        background-color: #f16d71;
-      }
-    }
-
-    @media (min-width: 750px) and (max-width: 950px) {
-
-      .app-grid__remove-icon {
-        position: absolute;
-        top: -7px;
-        right: -10px;
-        background-color: rgb(255, 255, 255);
-        color: #fcfbfb;
-        padding: 5px 5px 0px 5px;
-        cursor: pointer;
-        border-radius: 53%;
-        font-size: 14px;
-        z-index: 1;
-        background-color: #f16d71;
-      }
-    }
   `;
 
-
   /**
-   * Constructor for the class.
-   *
-   * Initializes instance variables.
-   *
-   * @constructor
+   * Constructor for the AppGrid element.
    */
   constructor() {
     super();
     this.appData = [];
     this.selectedIndex = -1; // -1 indicates no selection
-    this.sortableInstances = [];
+    this.showRemoveIconIndex = null; // To show remove icon for a specific item
     this.clickTimer = null;
     this.clickDelay = 300;
+    this.longPressTimer = 200;
+    this.longPressDuration = 500;
   }
 
-
   /**
-   * Executes when the element is added to the document's DOM.
-   * @memberof YourElement
-   * @function connectedCallback
-   * @returns {void}
+   * Lifecycle callback when the element is connected to the DOM.
    */
   connectedCallback() {
     super.connectedCallback();
     this.loadAppData();
-    this.initSortable();
-    this.boundHandleDocumentClick = this.handleDocumentClick.bind(this);
-    document.addEventListener('click', this.boundHandleDocumentClick);
+    document.addEventListener('click', this.handleDocumentClick);
+    document.addEventListener('mousedown', this.handleMouseDown);
+    document.addEventListener('mouseup', this.handleMouseUp);
+    document.addEventListener('mouseleave', this.handleMouseLeave);
   }
 
   /**
-   * Updates the component state and performs additional actions based on the changed properties.
-   *
-   * @param {Map<string, any>} changedProperties - The map of changed properties.
-   *
-   * @return {void}
+   * Prevents default behavior for drag events.
+   * @param {DragEvent} event - The drag event.
    */
-  updated(changedProperties) {
-    super.updated(changedProperties);
-    if (changedProperties.has('appData')) {
-      this.initSortable(); // Initialize sortable when appData changes
-    }
-  }
-
-
-  /**
-   * Initializes the sortable functionality for grid items within the container.
-   *
-   * @method initSortable
-   * @memberOf [Component]
-   *
-   * @return {void}
-   */
-  initSortable() {
-    // Select the container for the grid items
-    const appGrids = this.shadowRoot.querySelectorAll('.app-grid');
-    appGrids.forEach((appGrid) => {
-      const sortableInstance = new Sortable(appGrid, {
-        group: 'shared',
-        animation: 500,
-        draggable: '.app-grid__item', // Specify draggable items
-        onEnd: (evt) => this.updateOrder(evt)
-
-      });
-      this.sortableInstances.push(sortableInstance);
-
-    });
+  allowDrop(event) {
+    event.preventDefault();
   }
 
   /**
-   * Updates the order of items in the appData array based on the given event object.
-   * The event object should contain the properties 'oldIndex' and 'newIndex' which indicate the positions of the item in the array before and after moving.
-   * The method removes the item from the old position and inserts it at the new position to update the order.
-   * After updating the order, it posts the new order data to the server.
-   *
-   * @param {object} evt - The event object containing the properties 'oldIndex' and 'newIndex'.
-   * @return {void}
+   * Handles the drag event by setting the data being dragged.
+   * @param {DragEvent} event - The drag event.
+   * @param {number} index - The index of the dragged item.
    */
-  updateOrder(evt) {
-    // evt.newIndex and evt.oldIndex can be used to update the order
-    const itemMoved = this.appData.splice(evt.oldIndex, 1)[0];
-    this.appData.splice(evt.newIndex, 0, itemMoved);
+  drag(event, index) {
+    event.dataTransfer.setData("text/plain", index);
+  }
 
-    // Update the database with the new order
+  /**
+   * Handles the drop event by reordering the apps.
+   * @param {DragEvent} event - The drop event.
+   */
+  drop(event) {
+    event.preventDefault();
+    const fromIndex = event.dataTransfer.getData("text/plain");
+    const toIndex = event.target.dataset.index;
+    this.reorderApps(fromIndex, toIndex);
+  }
+
+  /**
+   * Reorders apps based on drag and drop.
+   * @param {number} fromIndex - The index from which the app is dragged.
+   * @param {number} toIndex - The index to which the app is dropped.
+   */
+  reorderApps(fromIndex, toIndex) {
+    const appsCopy = [...this.appData];
+    const [removedApp] = appsCopy.splice(fromIndex, 1);
+    appsCopy.splice(toIndex, 0, removedApp);
+    this.appData = appsCopy;
     this.postNewOrderToServer();
   }
 
   /**
-   * Posts a new order to the server.
-   *
-   * @returns {void}
+   * Posts the new order of apps to the server.
    */
   postNewOrderToServer() {
     const url = this.appUrl + "/update-app-order"; // Your API endpoint
@@ -189,7 +134,7 @@ class AppGrid extends LitElement {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(this.appData), // Send the updated appData array
+      body: JSON.stringify(this.appData),
     })
       .then(response => response.json())
       .then(data => {
@@ -200,11 +145,8 @@ class AppGrid extends LitElement {
       });
   }
 
-
   /**
-   * Loads the application data from an external source or a JSON attribute.
-   *
-   * @returns {void}
+   * Loads application data from an external source.
    */
   loadAppData() {
     // Fetch your data from an external source or set it from an attribute
@@ -218,14 +160,11 @@ class AppGrid extends LitElement {
   }
 
   /**
-   * Handles a single click event.
-   *
-   * @param {number} index - The index of the clicked element.
-   *
-   * @return {undefined}
+   * Handles a single click event on an app.
+   * @param {number} index - The index of the clicked app.
    */
   handleSingleClick(index) {
-    if (this.clickTimer === null) {
+    if (this.showRemoveIconIndex === null && this.clickTimer === null && this.longPressTimer === null) {
       this.clickTimer = setTimeout(() => {
         // Your single click logic here
         const selectedApp = this.appData[index];
@@ -239,17 +178,9 @@ class AppGrid extends LitElement {
     }
   }
 
-  deriveSlugFromUrl(url) {
-    // Implement logic to derive slug from the URL
-    // This is a placeholder, actual implementation depends on your URL structure
-    return url.split('/').pop(); // Example: gets the last part of the URL
-  }
-
   /**
-   * Handles double click event for a given index.
-   *
-   * @param {number} index - The index of the item being double clicked.
-   * @return {void}
+   * Handles a double click event on an app.
+   * @param {number} index - The index of the double-clicked app.
    */
   handleDoubleClick(index) {
     clearTimeout(this.clickTimer);
@@ -260,24 +191,28 @@ class AppGrid extends LitElement {
   }
 
   /**
-   * Handles the remove action for an item at the specified index.
-   * @param {Event} e - The event object.
-   * @param {number} index - The index of the item to be removed.
-   * @return {void}
+   * Handles removal of an app.
+   * @param {number} index - The index of the app to be removed.
    */
-  handleRemove(e, index) {
-    e.stopPropagation();
+  handleRemove(index) {
     const appId = this.appData[index].id;
-
     this.postAppDataToServer(appId);
-
     this.appData.splice(index, 1);
     this.selectedIndex = -1;
-    this.showRemoveIconIndex = null; // Reset the index after removal
-    this.requestUpdate(); // Request an update to re-render the component
+    this.showRemoveIconIndex = null;
   }
 
-  handleDocumentClick(event) {
+  /**
+   * Handles a click outside the context menu.
+   * @param {MouseEvent} event - The click event.
+   */
+  handleDocumentClick = (event) => {
+    // Check if a long press has occurred
+    if (this.longPressOccured) {
+      // Reset the flag
+      this.longPressOccured = false;
+      return;
+    }
     // Check if the click is outside the context menu
     const removeIcon = this.shadowRoot.querySelector('.app-grid__remove-icon');
     if (removeIcon && !removeIcon.contains(event.target)) {
@@ -287,15 +222,11 @@ class AppGrid extends LitElement {
     }
   }
 
-
   /**
-   * Sends the app data to the server to update the hidden apps list.
-   *
-   * @param {number} appId - The ID of the app to be hidden.
-   * @return {void}
+   * Posts data of the app to be hidden to the server.
+   * @param {string} appId - The ID of the app to be hidden.
    */
   postAppDataToServer(appId) {
-
     const url = this.appUrl + "/update-hide-apps";
     const appToHide = this.appData.find(app => app.id === appId);
 
@@ -303,6 +234,7 @@ class AppGrid extends LitElement {
       console.error('App not found');
       return;
     }
+
     fetch(url, {
       method: 'POST',
       headers: {
@@ -317,39 +249,87 @@ class AppGrid extends LitElement {
           // Handle error
         }
       })
-
       .catch((error) => {
         console.error('Error:', error);
       });
   }
 
   /**
-   * Renders the HTML for the application grid.
-   *
-   * @return {string} The rendered HTML string.
+   * Handles mouse down event on an app.
+   * @param {MouseEvent} event - The mouse down event.
+   * @param {number} index - The index of the app.
+   */
+  handleMouseDown = (event, index) => {
+    let appGridItem = event.target.closest('.app-grid__item');
+    if (!appGridItem) {
+      // If the target itself isn't an app grid item, check its parents
+      appGridItem = event.target.parentElement.closest('.app-grid__item');
+    }
+    if (appGridItem) {
+      this.longPressTimer = setTimeout(() => {
+        this.showContextMenu(index);
+        // Set a flag to indicate that a long press has occurred
+        this.longPressOccured = true;
+      }, this.longPressDuration);
+    }
+  }
+
+  /**
+   * Handles mouse up event.
+   */
+  handleMouseUp = () => {
+    clearTimeout(this.longPressTimer);
+    this.longPressTimer = null;
+  }
+
+  /**
+   * Handles mouse leave event.
+   * @param {number} index - The index of the app.
+   */
+  handleMouseLeave = (index) => {
+    clearTimeout(this.longPressTimer);
+    // Do something with the index if needed
+  }
+
+  /**
+   * Shows context menu for the app.
+   * @param {number} index - The index of the app.
+   */
+  showContextMenu(index) {
+    // Your logic to show the context menu
+    // For example, you can set a property to indicate which index's context menu to show
+    this.showRemoveIconIndex = index;
+    this.requestUpdate();
+  }
+
+  /**
+   * Renders the AppGrid element.
+   * @returns {TemplateResult} HTML template result.
    */
   render() {
     return html`
-      <div id="appGrid" class="app-grid">
+      <div class="app-grid">
         ${this.appData.map((app, index) => app.is_hidden !== 1 ? html`
           <div class="app-grid__item"
-               data-id="${app.id}"
+               data-index="${index}"
                @click="${() => this.handleSingleClick(index)}"
                @dblclick="${() => this.handleDoubleClick(index)}"
-          >
+               @mousedown="${(event) => this.handleMouseDown(event, index)}"
+               @dragstart="${(event) => this.drag(event, index)}"
+               @dragover="${this.allowDrop}"
+               @drop="${this.drop}"
+               draggable="true">
             ${this.showRemoveIconIndex === index
-              ? html`<span id="app-grid__remove-icon-${app.id}" class="app-grid__remove-icon"
-                           @click="${(e) => this.handleRemove(e, index)}"><sp-icon-close></sp-icon-close></span>`
+              ? html`<span class="app-grid__remove-icon"
+                           @click="${() => this.handleRemove(index)}"><sp-icon-close></sp-icon-close></span>`
               : ''}
             <dt-home-app-icon class="app-grid__icon"
                               name="${app.name}"
-                              icon="${app.icon}"
-            />
+                              icon="${app.icon}"></dt-home-app-icon>
           </div>` : '')}
       </div>
     `;
   }
-
 }
 
 customElements.define('dt-home-app-grid', AppGrid);
