ed985859d895a24e31495fe6f46fac77fc9999fc
Craig Wann <craig@codezone.io>
2024-03-23 14:01:26 -0500

    Removing DT styles from login page.

--- src/Services/Template.php
+++ src/Services/Template.php
@@ -6,94 +6,87 @@ use function DT\Home\Kucrut\Vite\enqueue_asset;
 use function DT\Home\plugin_path;
 use function DT\Home\view;
 
-class Template
-{
+class Template {
 
-    /**
-     * Allow access to blank template
-     * @return bool
-     */
-    public function blank_access(): bool
-    {
-        return true;
-    }
+	/**
+	 * Allow access to blank template
+	 * @return bool
+	 */
+	public function blank_access(): bool {
+		return true;
+	}
 
-    /**
-     * Start with a blank template
-     * @return void
-     */
-    public function template_redirect(): void
-    {
-        $path = get_theme_file_path( 'template-blank.php' );
-        include $path;
-        die();
-    }
+	/**
+	 * Start with a blank template
+	 * @return void
+	 */
+	public function template_redirect(): void {
+		$path = get_theme_file_path( 'template-blank.php' );
+		include $path;
+		die();
+	}
 
-    /**
-     * Enqueue CSS and JS assets
-     * @return void
-     */
-    public function wp_enqueue_scripts(): void
-    {
-        enqueue_asset(
-            plugin_path( '/dist' ),
-            'resources/js/plugin.js',
-            [
-                'handle' => 'dt_home',
-                'css-media' => 'all', // Optional.
-                'css-only' => false, // Optional. Set to true to only load style assets in production mode.
-                'in-footer' => false, // Optional. Defaults to false.
-            ]
-        );
-    }
+	/**
+	 * Enqueue CSS and JS assets
+	 * @return void
+	 */
+	public function wp_enqueue_scripts(): void {
+		wp_dequeue_style( 'site-css' );
+		enqueue_asset(
+			plugin_path( '/dist' ),
+			'resources/js/plugin.js',
+			[
+				'handle'    => 'dt_home',
+				'css-media' => 'all', // Optional.
+				'css-only'  => false, // Optional. Set to true to only load style assets in production mode.
+				'in-footer' => false, // Optional. Defaults to false.
+			]
+		);
+	}
 
 
-    /**
-     * Render the header
-     * @return void
-     */
-    public function header()
-    {
-        wp_head();
-    }
+	/**
+	 * Render the header
+	 * @return void
+	 */
+	public function header() {
+		wp_head();
+	}
 
-    public function cloak_visibility()
-    {
-        ?>
+	public function cloak_visibility() {
+		?>
         <style>
             .cloak {
                 visibility: hidden;
             }
         </style>
-        <?php
-    }
+		<?php
+	}
 
-    /**
-     * Render the template
-     *
-     * @param $template
-     * @param $data
-     *
-     * @return mixed
-     */
-    public function render( $template, $data )
-    {
-        add_action( 'template_redirect', [ $this, 'template_redirect' ] );
-        add_filter( 'dt_blank_access', [ $this, 'blank_access' ] );
-        add_action( 'dt_blank_head', [ $this, 'header' ] );
-        add_action( 'dt_blank_footer', [ $this, 'footer' ] );
-        add_action( 'wp_enqueue_scripts', [ $this, 'wp_enqueue_scripts' ] );
-        add_action( 'wp_head', [ $this, 'cloak_visibility' ] );
+	/**
+	 * Render the template
+	 *
+	 * @param $template
+	 * @param $data
+	 *
+	 * @return mixed
+	 */
+	public function render( $template, $data ) {
+		add_action( 'template_redirect', [ $this, 'template_redirect' ] );
+		add_filter( 'dt_blank_access', [ $this, 'blank_access' ] );
+		add_action( 'dt_blank_head', [ $this, 'header' ] );
+		add_action( 'dt_blank_footer', [ $this, 'footer' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'wp_enqueue_scripts' ], 1000 );
+		add_action( 'wp_head', [ $this, 'cloak_visibility' ] );
 
-        return view()->render( $template, $data );
-    }
+		return view()->render( $template, $data );
+	}
 
-    /**
-     * Render the footer
-     * @return void
-     */
-    public function footer()
-    {
-        wp_footer();
-    }
+	/**
+	 * Render the footer
+	 * @return void
+	 */
+	public function footer() {
+		wp_footer();
+	}
 }
