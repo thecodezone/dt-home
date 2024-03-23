82182e48e673a6de35b2e45064d469770a4b5610
Craig Wann <craig@codezone.io>
2024-03-23 14:00:43 -0500

    Updating spectrum.

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -1,10 +1,10 @@
-import "@disciple.tools/web-components";
-
 /**
  * Import Spectrum Web Components
  */
 import '@spectrum-web-components/theme/sp-theme.js';
 import '@spectrum-web-components/theme/src/themes.js';
+import "@disciple.tools/web-components";
+
 import '@spectrum-web-components/menu/sp-menu.js';
 import '@spectrum-web-components/menu/sp-menu-group.js';
 import '@spectrum-web-components/menu/sp-menu-item.js';
@@ -13,6 +13,8 @@ import '@spectrum-web-components/overlay/sp-overlay.js';
 import '@spectrum-web-components/icon/sp-icon.js';
 import '@spectrum-web-components/icons-ui/icons/sp-icon-triple-gripper.js';
 import '@spectrum-web-components/action-group/sp-action-group.js';
+import '@spectrum-web-components/button-group/sp-button-group.js';
+import '@spectrum-web-components/button/sp-button.js';
 import '@spectrum-web-components/button/sp-clear-button.js';
 import '@spectrum-web-components/button/sp-close-button.js';
 import "@spectrum-web-components/progress-circle/sp-progress-circle.js";
@@ -20,13 +22,14 @@ import '@spectrum-web-components/action-menu/sp-action-menu.js';
 import '@spectrum-web-components/tooltip/sp-tooltip.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-help.js';
 import '@spectrum-web-components/overlay/sync/overlay-trigger.js';
-import '@spectrum-web-components/button/sp-button.js';
 import '@spectrum-web-components/action-menu/sp-action-menu.js';
 import '@spectrum-web-components/tooltip/sp-tooltip.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-help.js';
 import '@spectrum-web-components/overlay/sync/overlay-trigger.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-view-grid.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-close.js';
+import '@spectrum-web-components/field-group/sp-field-group.js';
+import '@spectrum-web-components/textfield/sp-textfield.js';
 
 /**
  * Web Components
@@ -46,15 +49,15 @@ import "../css/plugin.css";
  * Imports
  */
 import {loaded} from "./helpers.js";
-import submitFormOnEnter from "./dom-hooks/submit-form-on-enter.js";
 import handleDomLoaded from "./dom-hooks/handle-dom-loaded.js";
 import decloak from "./dom-hooks/decloak.js";
+import submitFormOnEnter from "./dom-hooks/submit-form-on-enter.js";
 
 /**
  * Bootstrap the application
  */
 loaded((document) => {
+    document.querySelectorAll("form").forEach(submitFormOnEnter);
     document.querySelectorAll(".cloak").forEach(decloak);
     document.querySelectorAll('body').forEach(handleDomLoaded);
-    document.querySelectorAll('form').forEach(submitFormOnEnter);
 });
