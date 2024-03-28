29ffb7cb28ff3b1894ca0097601b31ec581f8e85
ancy2023 <127301741+ancy2023@users.noreply.github.com>
2024-03-28 10:25:25 +0530

    app clicks do not work
    commit to app clicks do not work

--- src/Controllers/MagicLink/HomeController.php
+++ src/Controllers/MagicLink/HomeController.php
@@ -10,22 +10,22 @@ use function DT\Home\template;
 
 class HomeController
 {
-    public function show(Request $request, Response $response, $key)
+    public function show( Request $request, Response $response, $key )
     {
         $user = wp_get_current_user();
-        $subpage_url = magic_url('subpage', $key);
+        $subpage_url = magic_url( 'subpage', $key );
 
-        $apps_array = get_user_option('dt_home_apps', wp_get_current_user());
+        $apps_array = get_user_option( 'dt_home_apps', wp_get_current_user() );
         //dd($apps_array);
         // Fallback to default option if user option is not set
-        if ($apps_array === false) {
-            $apps_array = get_option('dt_home_apps');
+        if ( $apps_array === false ) {
+            $apps_array = get_option( 'dt_home_apps' );
         }
 
-        $data = json_encode($apps_array);
-        $hidden_data = json_encode($apps_array);
+        $data = json_encode( $apps_array );
+        $hidden_data = json_encode( $apps_array );
         //dd($data);
-        $app_url = magic_url('', $key);
+        $app_url = magic_url( '', $key );
         $magic_link = $app_url . '/share';
 
         return template('index', compact(
@@ -38,21 +38,21 @@ class HomeController
         ));
     }
 
-    public function show_hidden_apps(Request $request, Response $response, $key)
+    public function show_hidden_apps( Request $request, Response $response, $key )
     {
         $user = wp_get_current_user();
-        $subpage_url = magic_url('subpage', $key);
+        $subpage_url = magic_url( 'subpage', $key );
         $magic_link = magic_url();
 
-        $apps_array = get_user_option('dt_home_apps', []);
+        $apps_array = get_user_option( 'dt_home_apps', [] );
 
         // Fallback to default option if user option is not set
-        if (!$apps_array) {
-            $apps_array = get_option('dt_home_apps', []);
+        if ( !$apps_array ) {
+            $apps_array = get_option( 'dt_home_apps', [] );
         }
-        $data = json_encode($apps_array);
+        $data = json_encode( $apps_array );
 
-        $app_url = magic_url('', $key);
+        $app_url = magic_url( '', $key );
 
         return template('hidden-apps', compact(
             'user',
@@ -63,114 +63,114 @@ class HomeController
         ));
     }
 
-    public function data(Request $request, Response $response, $key)
+    public function data( Request $request, Response $response, $key )
     {
         $user = wp_get_current_user();
         $data = [
             'user_login' => $user->user_login,
         ];
-        $response->setContent($data);
+        $response->setContent( $data );
 
         return $response;
     }
 
-    public function update_hide_app(Request $request, Response $response, $key)
+    public function update_hide_app( Request $request, Response $response, $key )
     {
         $data = $request->json()->all();
 
         // Assuming $data contains 'id' and 'is_hidden'
         $app_id = $data['id'];
 
-        $apps_array = get_user_option('dt_home_apps', get_current_user_id());
+        $apps_array = get_user_option( 'dt_home_apps', get_current_user_id() );
 
         // Fallback to default option if user option is not set
-        if ($apps_array === false) {
-            $apps_array = get_option('dt_home_apps');
+        if ( $apps_array === false ) {
+            $apps_array = get_option( 'dt_home_apps' );
         }
 
         // Find the app with the specified ID and update its 'is_hidden' status
-        foreach ($apps_array as $key => $app) {
-            if (isset($app['id']) && $app['id'] == $app_id) {
+        foreach ( $apps_array as $key => $app ) {
+            if ( isset( $app['id'] ) && $app['id'] == $app_id ) {
                 $apps_array[$key]['is_hidden'] = 1; // Set 'is_hidden' to 1 (hide)
                 break; // Exit the loop once the app is found and updated
             }
         }
         // Save the updated array back to the option
 
-        update_user_option(get_current_user_id(), 'dt_home_apps', $apps_array);
+        update_user_option( get_current_user_id(), 'dt_home_apps', $apps_array );
 
-        $response_data = ['message' => 'App visibility updated'];
+        $response_data = [ 'message' => 'App visibility updated' ];
 
-        $response->setContent(json_encode($response_data));
+        $response->setContent( json_encode( $response_data ) );
 
         return $response;
     }
 
 
-    public function update_unhide_app(Request $request, Response $response, $key)
+    public function update_unhide_app( Request $request, Response $response, $key )
     {
         $data = $request->json()->all();
 
         // Assuming $data contains 'id' and 'is_hidden'
         $app_id = $data['id'];
 
-        $apps_array = get_user_option('dt_home_apps', get_current_user_id());
+        $apps_array = get_user_option( 'dt_home_apps', get_current_user_id() );
 
         // Fallback to default option if user option is not set
-        if ($apps_array === false) {
-            $apps_array = get_option('dt_home_apps');
+        if ( $apps_array === false ) {
+            $apps_array = get_option( 'dt_home_apps' );
         }
 
         // Find the app with the specified ID and update its 'is_hidden' status
-        foreach ($apps_array as $key => $app) {
-            if (isset($app['id']) && $app['id'] == $app_id) {
+        foreach ( $apps_array as $key => $app ) {
+            if ( isset( $app['id'] ) && $app['id'] == $app_id ) {
                 $apps_array[$key]['is_hidden'] = 0; // Set 'is_hidden' to 1 (hide)
                 break; // Exit the loop once the app is found and updated
             }
         }
         // Save the updated array back to the option
-        update_user_option(get_current_user_id(), 'dt_home_apps', $apps_array);
+        update_user_option( get_current_user_id(), 'dt_home_apps', $apps_array );
 
-        $response_data = ['message' => 'App visibility updated'];
+        $response_data = [ 'message' => 'App visibility updated' ];
 
-        $response->setContent(json_encode($response_data));
+        $response->setContent( json_encode( $response_data ) );
 
         return $response;
     }
 
-    public function update_app_order(Request $request, Response $response, $key): Response
+    public function update_app_order( Request $request, Response $response, $key ): Response
     {
         $data = $request->json()->all();
         // Iterate through each app in the data
-        foreach ($data as $key => $app) {
+        foreach ( $data as $key => $app ) {
             // Update the 'sort' field for each app based on its position in the array
             $data[$key]['sort'] = $key + 1;
         }
         // Save the updated app order back to the database or storage
-        update_user_option(get_current_user_id(), 'dt_home_apps', $data);
+        update_user_option( get_current_user_id(), 'dt_home_apps', $data );
 
-        $response_data = ['message' => 'App order updated'];
+        $response_data = [ 'message' => 'App order updated' ];
 
-        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set( 'Content-Type', 'application/json' );
 
-        $response->setContent(json_encode($response_data));
+        $response->setContent( json_encode( $response_data ) );
 
         return $response;
     }
 
-    public function open_app($slug)
+    public function open_app( $slug )
     {
-        $apps_array = get_option('dt_home_apps', []);
+        $apps_array = get_option( 'dt_home_apps', [] );
 
         $desired_app = null;
 
-        foreach ($apps_array as $app) {
-            if ((is_array($app) && $app['id'] == $slug) || (is_object($app) && $app->id == $slug)) {
+        foreach ( $apps_array as $app ) {
+            if ( ( is_array( $app ) && $app['id'] == $slug ) || ( is_object( $app ) && $app->id == $slug ) ) {
                 $desired_app = $app;
                 break;
             }
         }
 
-        return template('web-view', compact('desired_app'));
+        return template( 'web-view', compact( 'desired_app' ) );
     }
 }
--- src/Providers/RouterServiceProvider.php
+++ src/Providers/RouterServiceProvider.php
@@ -10,53 +10,58 @@ use DT\Home\Illuminate\Http\Response;
 use function DT\Home\namespace_string;
 use function DT\Home\routes_path;
 
-class RouterServiceProvider extends ServiceProvider {
-	/**
-	 * Do any setup needed before the theme is ready.
-	 * DT is not yet registered.
-	 */
-	public function register(): void {
-		Router::register( [
-			'container' => $this->container,
-		] );
+class RouterServiceProvider extends ServiceProvider
+{
+    /**
+     * Do any setup needed before the theme is ready.
+     * DT is not yet registered.
+     */
+    public function register(): void
+    {
+        Router::register([
+            'container' => $this->container,
+        ]);
 
-		add_filter( Router\namespace_string( "routes" ), [ $this, 'include_route_file' ], 1 );
-		add_action( Router\namespace_string( "render" ), [ $this, 'render_response' ], 10, 2 );
-	}
+        add_filter( Router\namespace_string( "routes" ), [ $this, 'include_route_file' ], 1 );
+        add_action( Router\namespace_string( "render" ), [ $this, 'render_response' ], 10, 2 );
+    }
 
-	/**
-	 * DT is ready. Do any setup needed after the theme is ready.
-	 */
-	public function boot(): void {
-		if ( is_admin() ) {
-			return;
-		}
+    /**
+     * DT is ready. Do any setup needed after the theme is ready.
+     */
+    public function boot(): void
+    {
+        if ( is_admin() ) {
+            return;
+        }
 
-		apply_filters( namespace_string( 'middleware' ), $this->container->make( Stack::class ) )
-			->run();
-	}
+        apply_filters( namespace_string( 'middleware' ), $this->container->make( Stack::class ) )
+            ->run();
+    }
 
-	/**
-	 * Register the routes for the application.
-	 *
-	 * @param Routes $r
-	 *
-	 * @return Routes
-	 */
-	public function include_route_file( Routes $r ): RouteCollector {
+    /**
+     * Register the routes for the application.
+     *
+     * @param Routes $r
+     *
+     * @return Routes
+     */
+    public function include_route_file( Routes $r ): RouteCollector
+    {
 
-		include routes_path( 'web.php' );
+        include routes_path( 'web.php' );
 
-		return $r;
-	}
+        return $r;
+    }
 
-	public function render_response( Response $response ) {
-		if ( apply_filters( 'dt_blank_access', false ) ) {
-			add_action( 'dt_blank_body', function () use ( $response ) {
-				echo $response->getContent();
-			} );
-		} else {
-			$response->send();
-		}
-	}
+    public function render_response( Response $response )
+    {
+        if ( apply_filters( 'dt_blank_access', false ) ) {
+            add_action('dt_blank_body', function () use ( $response ) {
+                $response->getContent();
+            });
+        } else {
+            $response->send();
+        }
+    }
 }
