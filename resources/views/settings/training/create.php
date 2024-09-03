1d55a1174d85c84d9784249235081768a60da061
Muniraj <muni20002raj@gmail.com>
2024-09-03 16:04:30 +0530

    Helpful training video edit tips
    implemented the styles for tooltip and add the translate function( <?php esc_html_e('') ?>) for all labels for training tab.

--- resources/views/settings/training/create.php
+++ resources/views/settings/training/create.php
@@ -7,34 +7,54 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
     <table class="widefat striped" id="ml_email_main_col_config">
     <thead>
     <tr>
-        <th>Training Video</th>
+        <th><?php esc_html_e('Training Videos') ?></th>
         <th></th>
         <th></th>
     </tr>
     </thead>
     <tbody>
     <tr>
-        <td style="vertical-align: middle;">Name <span title="Enter the name of the training video.">[?]</span></td>
+        <td style="vertical-align: middle;">
+            <?php esc_html_e('Name') ?>
+            <span class="tooltip">[?]
+                <span class="tooltiptext"><?php esc_html_e('Enter the name of the training video.') ?></span>
+            </span>
+        </td>
         <td colspan="2">
             <input style="min-width: 100%;" class="form-control" type="text" name="name" id="name"  pattern=".*\S+.*" title="The name cannot be empty or just whitespace."  required/>
         </td>
     </tr>
     <tr>
-        <td style="vertical-align: middle;">Embed Video <span title="Paste the embed code for the video.">[?]</span></td>
+        <td style="vertical-align: middle;">
+            <?php esc_html_e('Embed Video') ?>
+            <span class="tooltip">[?]
+            <span class="tooltiptext"><?php esc_html_e('Paste the embed code for the video.') ?></span>
+        </span>
+        </td>
         <td colspan="2">
             <textarea style="min-width: 100%;" class="form-control" name="embed_video" id="embed_video"
                       oninput="this.setCustomValidity(this.value.trim() === '' ? 'The embed Video cannot be empty or just whitespace.' : '')"   required></textarea>
         </td>
     </tr>
     <tr>
-        <td style="vertical-align: middle;">Anchor <span title="Specify the anchor text for the video.">[?]</span></td>
+        <td style="vertical-align: middle;">
+            <?php esc_html_e('Anchor') ?>
+            <span class="tooltip">[?]
+            <span class="tooltiptext"><?php esc_html_e("Specify the anchor text for the video.")?> </span>
+        </span>
+        </td>
         <td colspan="2">
             <input style="min-width: 100%;" class="form-control" type="text" name="anchor" id="anchor"
                    pattern=".*\S+.*" title="The name cannot be empty or just whitespace." required/>
         </td>
     </tr>
     <tr>
-        <td style="vertical-align: middle;">Sort <span title="Set the sort order for the video.">[?]</span></td>
+        <td style="vertical-align: middle;">
+            <?php esc_html_e('Sort') ?>
+            <span class="tooltip">[?]
+            <span class="tooltiptext"> <?php esc_html_e( "Set the sort order for the video.")?></span>
+        </span>
+        </td>
         <td colspan="2">
             <input style="min-width: 100%;" type="number" name="sort" id="sort" required/>
         </td>
--- resources/views/settings/training/edit.php
+++ resources/views/settings/training/edit.php
@@ -9,7 +9,7 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
     <table class="widefat striped" id="ml_email_main_col_config">
     <thead>
     <tr>
-        <th>Training Videos</th>
+        <th><?php esc_html_e('Training Videos') ?></th>
         <th></th>
         <th></th>
         <th></th>
@@ -17,35 +17,55 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
     </thead>
     <tbody>
     <tr>
-        <td style="vertical-align: middle;">Name <span title="Enter the name of the training video.">[?]</span></td>
+      <td style="vertical-align: middle;">
+            <?php esc_html_e('Name') ?>
+            <span class="tooltip">[?]
+                <span class="tooltiptext"><?php esc_html_e('Enter the name of the training video.') ?></span>
+            </span>
+        </td>
         <td colspan="3">
             <input style="min-width: 100%;" type="text" name="name" id="name" class="form-control"
                    value="<?php echo esc_attr( $existing_data['name'] ); ?>" pattern=".*\S+.*" title="The name cannot be empty or just whitespace." required>
         </td>
     </tr>
     <tr>
-        <td style="vertical-align: middle;">Embed Video <span title="Paste the embed code for the video.">[?]</span></td>
-        <td colspan="3">
-            <textarea style="min-width: 100%;" class="form-control" name="embed_video" id="embed_video"
-                      oninput="this.setCustomValidity(this.value.trim() === '' ? 'The video embed cannot be empty or just whitespace.' : '')"
-                      required><?php echo stripslashes( esc_html( $existing_data['embed_video'] ) ); ?>
-            </textarea>
-        </td>
-    </tr>
-    <tr>
-        <td style="vertical-align: middle;">Anchor <span title="Specify the anchor text for the video.">[?]</span></td>
-        <td colspan="2">
-            <input style="min-width: 100%;" class="form-control" type="text" name="anchor" id="anchor"
-                   value="<?php echo esc_attr( $existing_data['anchor'] ); ?>" pattern=".*\S+.*" title="The name cannot be empty or just whitespace." required/>
-        </td>
-    </tr>
-    <tr>
-        <td style="vertical-align: middle;">Sort <span title="Set the sort order for the video.">[?]</span></td>
-        <td colspan="3">
-            <input style="min-width: 100%;" type="number" name="sort" id="sort" class="form-control"
-                   value="<?php echo esc_attr( $existing_data['sort'] ); ?>" required>
-        </td>
-    </tr>
+    <td style="vertical-align: middle;">
+        <?php esc_html_e('Embed Video') ?>
+        <span class="tooltip">[?]
+            <span class="tooltiptext"><?php esc_html_e('Paste the embed code for the video.') ?></span>
+        </span>
+    </td>
+    <td colspan="3">
+        <textarea style="min-width: 100%;" class="form-control" name="embed_video" id="embed_video"
+                  oninput="this.setCustomValidity(this.value.trim() === '' ? 'The video embed cannot be empty or just whitespace.' : '')"
+                  required><?php echo stripslashes( esc_html( $existing_data['embed_video'] ) ); ?>
+        </textarea>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: middle;">
+        <?php esc_html_e('Anchor') ?>
+        <span class="tooltip">[?]
+            <span class="tooltiptext"><?php esc_html_e("Specify the anchor text for the video.")?> </span>
+        </span>
+    </td>
+    <td colspan="3">
+        <input style="min-width: 100%;" class="form-control" type="text" name="anchor" id="anchor"
+               value="<?php echo esc_attr( $existing_data['anchor'] ); ?>" pattern=".*\S+.*" title="The name cannot be empty or just whitespace." required/>
+    </td>
+</tr>
+<tr>
+    <td style="vertical-align: middle;">
+          <?php esc_html_e('Sort') ?>
+        <span class="tooltip">[?]
+            <span class="tooltiptext"> <?php esc_html_e( "Set the sort order for the video.")?></span>
+        </span>
+    </td>
+    <td colspan="3">
+        <input style="min-width: 100%;" type="number" name="sort" id="sort" class="form-control"
+               value="<?php echo esc_attr( $existing_data['sort'] ); ?>" required>
+    </td>
+</tr>
     </tbody>
 </table>
 
