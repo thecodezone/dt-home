1e52bb0a32a574ae082a24883afe6c4e4aaf3768
kodinkat <me@karlkatewu.com>
2024-09-11 10:18:31 +0100

    Reintroduce Clean Up test as empty placeholder

---
+++ tests/CleanUp.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use WP_UnitTestCase;
+
+class CleanUp extends WP_UnitTestCase {
+
+    public static function tearDownAfterClass(): void {
+    }
+
+    public function test_dummy() {
+        $this->assertTrue( true );
+    }
+}
