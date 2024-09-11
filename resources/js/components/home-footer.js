7e8e0da2a9515058b8618b9e95ac799a2985afda
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2024-09-11 09:50:20 +0530

    added magic url

--- resources/js/components/home-footer.js
+++ resources/js/components/home-footer.js
@@ -6,6 +6,7 @@ import '@spectrum-web-components/overlay/overlay-trigger.js'
 import { customElement } from 'lit-element'
 import './app-menu.js'
 import './app-menu-item.js'
+import { magic_url } from '../helpers.js'
 
 @customElement('dt-home-footer')
 class HomeFooter extends LitElement {
@@ -24,210 +25,194 @@ class HomeFooter extends LitElement {
 
     static get styles() {
         return css`
-            :host {
-                --mod-divider-thickness: 0px;
-                --spectrum-spacing-300: 0px;
-                --spectrum-spacing-200: 0px;
-                --spectrum-dialog-confirm-divider-block-spacing-start: var(
-                    --spectrum-spacing-300
-                );
-                --spectrum-dialog-confirm-divider-block-spacing-end: var(
-                    --spectrum-spacing-200
-                );
-                --mod-dialog-confirm-padding-grid: 0px;
-                --spectrum-dialog-confirm-padding-grid: 0px;
-                //--spectrum-spacing-50: 0px;
-                //--spectrum-dialog-confirm-description-padding: var(
-                //--spectrum-spacing-50);
-            }
+      :host {
+        --mod-divider-thickness: 0px;
+        --spectrum-spacing-300: 0px;
+        --spectrum-spacing-200: 0px;
+        --spectrum-dialog-confirm-divider-block-spacing-start: var(
+          --spectrum-spacing-300
+        );
+        --spectrum-dialog-confirm-divider-block-spacing-end: var(
+          --spectrum-spacing-200
+        );
+        --mod-dialog-confirm-padding-grid: 0px;
+        --spectrum-dialog-confirm-padding-grid: 0px;
+      );
+      }
 
-            .footer-container {
-                padding: 5px;
-                display: flex;
-                justify-content: right;
-                bottom: 20px;
-            }
+      .footer-container {
+        padding: 5px;
+        display: flex;
+        justify-content: right;
+        bottom: 20px;
+      }
 
-            .trigger-button {
-                border: none;
-                color: white;
-                cursor: pointer;
-                background-color: #1a73e8; /* Blue background */
-                border-radius: 50%; /* Make the button circular */
-                width: 40px; /* Set width */
-                height: 40px; /* Set height */
-                display: flex; /* Center the icon */
-                justify-content: center; /* Center the icon horizontally */
-                align-items: center; /* Center the icon vertically */
-            }
+      .trigger-button {
+        border: none;
+        color: white;
+        cursor: pointer;
+        background-color: #1a73e8; /* Blue background */
+        border-radius: 50%; /* Make the button circular */
+        width: 40px; /* Set width */
+        height: 40px; /* Set height */
+        display: flex; /* Center the icon */
+        justify-content: center; /* Center the icon horizontally */
+        align-items: center; /* Center the icon vertically */
+      }
 
-            .trigger-button sp-icon-add {
-                width: 36px; /* Icon width */
-                height: 36px; /* Icon height */
-            }
+      .trigger-button sp-icon-add {
+        width: 36px; /* Icon width */
+        height: 36px; /* Icon height */
+      }
 
-            //.custom-dialog-overlay {
-            //    position: fixed;
-            //    bottom: 18px;
-            //    top: 380px; /* Adjusted for better height */
-            //    right: 25px;
-            //    left: 570px;
-            //    transform: none;
-            //    z-index: 9999;
-            //    border: none; /* Remove any border */
-            //    height: 200px; /* Let the content dictate the height */
-            //    max-height: 500px; /* Set max height to avoid overflow */
-            //    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15); /* Optional: Add a subtle shadow */
-            //}
 
-            sp-dialog {
-                background-color: white;
-                border: none; /* Remove any border */
-                box-shadow: none; /* Remove any shadow */
-                height: 200px; /* Let the content dictate the height */
-                padding: 0; /* Remove default padding */
-                overflow: hidden; /* Hide overflow */
-                margin-right: -24px;
-                margin-bottom: -41px;
-            }
+      sp-dialog {
+        background-color: white;
+        border: none; /* Remove any border */
+        box-shadow: none; /* Remove any shadow */
+        height: 200px; /* Let the content dictate the height */
+        padding: 0; /* Remove default padding */
+        overflow: hidden; /* Hide overflow */
+        margin-right: -24px;
+        margin-bottom: -41px;
+      }
 
-            .app-row {
-                display: flex;
-                align-items: center;
-                border-bottom: 1px solid lightgray; /* Add bottom border */
-            }
+      .app-row {
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid lightgray; /* Add bottom border */
+      }
 
-            .app-row:last-child {
-                border-bottom: none; /* Remove bottom border for the last item */
-            }
+      .app-row:last-child {
+        border-bottom: none; /* Remove bottom border for the last item */
+      }
 
-            .app-icon {
-                width: 30px;
-                height: 30px;
-                margin-right: 8px;
-            }
+      .app-icon {
+        width: 30px;
+        height: 30px;
+        margin-right: 8px;
+      }
 
-            .material-icons.app-icon {
-                font-size: 30px;
-                line-height: 30px;
-            }
+      .material-icons.app-icon {
+        font-size: 30px;
+        line-height: 30px;
+      }
 
-            .app-name {
-                flex: 1; /* Ensure the name takes the remaining space */
-                color: black; /* Ensure text color is black */
-            }
+      .app-name {
+        flex: 1; /* Ensure the name takes the remaining space */
+        color: black; /* Ensure text color is black */
+      }
 
-            .app-name:hover {
-                color: hsla(216, 100%, 50%, 1);
-            }
+      .app-name:hover {
+        color: hsla(216, 100%, 50%, 1);
+      }
 
-            .reset-apps {
-                color: #ffffff;
+      .reset-apps {
+        color: #ffffff;
 
-                background-color: #e94f54;
-                --system-spectrum-actionbutton-background-color-default: var(
-                    --background-color
-                );
-                --spectrum-component-height-100: 19px;
-                --spectrum-font-size-100: 10px;
-                left: -264px;
-                top: 204px;
-            }
+        background-color: #e94f54;
+        --system-spectrum-actionbutton-background-color-default: var(
+          --background-color
+        );
+        --spectrum-component-height-100: 19px;
+        --spectrum-font-size-100: 10px;
+        left: -264px;
+        top: 204px;
+      }
 
-            /* Mobile */
-            @media (max-width: 600px) {
-                .footer-container {
-                    position: fixed;
-                    width: 100%;
-                    display: flex;
-                    right: 10px;
-                }
+      /* Mobile */
+      @media (max-width: 600px) {
+        .footer-container {
+          position: fixed;
+          width: 100%;
+          display: flex;
+          right: 10px;
+        }
 
-                .custom-dialog-overlay {
-                    left: calc(100vw - 72vw) !important;
-                    right: 0;
-                    bottom: 25px;
-                    top: auto;
-                    width: 60vw;
-                    max-height: 400px; /* Set max height */
-                    overflow: hidden; /* Hide overflow */
-                }
+        .custom-dialog-overlay {
+          left: calc(100vw - 72vw) !important;
+          right: 0;
+          bottom: 25px;
+          top: auto;
+          width: 60vw;
+          max-height: 400px; /* Set max height */
+          overflow: hidden; /* Hide overflow */
+        }
 
-                sp-dialog {
-                    background-color: white;
-                    //  height: auto; /* Let the content dictate the height */
-                    height: 200px;
-                }
-            }
+        sp-dialog {
+          background-color: white;
+          height: 200px;
+        }
+      }
 
-            /* Tablet */
-            @media (min-width: 601px) and (max-width: 962px) {
-                .footer-container {
-                    position: fixed;
-                    width: 100%;
-                    display: flex;
-                    right: 10px;
-                }
+      /* Tablet */
+      @media (min-width: 601px) and (max-width: 962px) {
+        .footer-container {
+          position: fixed;
+          width: 100%;
+          display: flex;
+          right: 10px;
+        }
 
-                .custom-dialog-overlay {
-                    left: calc(100vw - 350px) !important;
-                    top: auto;
-                    width: 100px;
-                }
-            }
+        .custom-dialog-overlay {
+          left: calc(100vw - 350px) !important;
+          top: auto;
+          width: 100px;
+        }
+      }
 
-            /* Desktop */
-            @media (min-width: 963px) and (max-width: 1920px) {
-                .footer-container {
-                    position: fixed;
-                    width: 100%;
-                    display: flex;
-                    right: 10px;
-                }
+      /* Desktop */
+      @media (min-width: 963px) and (max-width: 1920px) {
+        .footer-container {
+          position: fixed;
+          width: 100%;
+          display: flex;
+          right: 10px;
+        }
 
-                .custom-dialog-overlay {
-                    left: calc(100vw - 350px) !important;
-                    top: auto;
-                    width: 100px;
-                }
-            }
+        .custom-dialog-overlay {
+          left: calc(100vw - 350px) !important;
+          top: auto;
+          width: 100px;
+        }
+      }
 
-            sp-menu-item {
-                background-color: transparent !important;
-                border-left: none !important;
-                transition: none !important;
-                padding-left: 0 !important;
-                outline: none !important; /* Ensure no focus outline */
-                box-shadow: none !important; /* Ensure no box shadow */
-            }
+      sp-menu-item {
+        background-color: transparent !important;
+        border-left: none !important;
+        transition: none !important;
+        padding-left: 0 !important;
+        outline: none !important; /* Ensure no focus outline */
+        box-shadow: none !important; /* Ensure no box shadow */
+      }
 
-            sp-menu-item::part(heading),
-            sp-menu-item::part(indicator),
-            sp-menu-item::part(checkmark) {
-                display: none !important;
-            }
+      sp-menu-item::part(heading),
+      sp-menu-item::part(indicator),
+      sp-menu-item::part(checkmark) {
+        display: none !important;
+      }
 
-            sp-menu-item:hover,
-            sp-menu-item:focus,
-            sp-menu-item:active,
-            sp-menu-item:focus-visible {
-                border-left: none !important; /* Remove left border on all states */
-                background-color: lightgray !important; /* Change hover background color as needed */
-                outline: none !important; /* Remove focus outline */
-                box-shadow: none !important; /* Ensure no box shadow */
-            }
+      sp-menu-item:hover,
+      sp-menu-item:focus,
+      sp-menu-item:active,
+      sp-menu-item:focus-visible {
+        border-left: none !important; /* Remove left border on all states */
+        background-color: lightgray !important; /* Change hover background color as needed */
+        outline: none !important; /* Remove focus outline */
+        box-shadow: none !important; /* Ensure no box shadow */
+      }
 
-            sp-menu-item[focused] {
-                outline: none !important;
-                border-left: none !important; /* Remove blue left border */
-                box-shadow: none !important; /* Ensure no box shadow */
-            }
+      sp-menu-item[focused] {
+        outline: none !important;
+        border-left: none !important; /* Remove blue left border */
+        box-shadow: none !important; /* Ensure no box shadow */
+      }
 
-            .no-data {
-                color: gray;
-                padding: 10px; /* Add padding for space */
-            }
-        `
+      .no-data {
+        color: gray;
+        padding: 10px; /* Add padding for space */
+      }
+    `
     }
 
     get hiddenApps() {
@@ -248,7 +233,7 @@ class HomeFooter extends LitElement {
     }
 
     postAppDataToServer(appSlug) {
-        const url = this.appUrl + '/un-hide-app'
+        const url = magic_url('unhide')
         const appToHide = this.appData.find((app) => app.slug === appSlug)
 
         if (!appToHide) {
@@ -265,6 +250,7 @@ class HomeFooter extends LitElement {
         })
             .then((response) => {
                 if (response.ok) {
+                    console.log(response)
                     window.location.reload()
                 } else {
                     // Handle error
@@ -297,7 +283,7 @@ class HomeFooter extends LitElement {
         )
 
         if (confirmDelete) {
-            fetch(this.appUrl + '/reset-apps', {
+            fetch(magic_url('reset-apps'), {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -357,7 +343,7 @@ class HomeFooter extends LitElement {
                 @import url('https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css');
             </style>
             <div class="footer-container">
-                <overlay-trigger placement="top">
+                <overlay-trigger type="modal" placement="top">
                     <button slot="trigger" class="trigger-button">
                         <sp-icon-add></sp-icon-add>
                     </button>
