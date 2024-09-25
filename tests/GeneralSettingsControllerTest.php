9ad63bbad06c43bd929e4326ad80b43143f79503
Muniraj <muni20002raj@gmail.com>
2024-09-25 10:54:32 +0530

    Updated regression tests for new field in 'Apps' menu addition.

--- tests/GeneralSettingsControllerTest.php
+++ tests/GeneralSettingsControllerTest.php
@@ -51,4 +51,34 @@ class GeneralSettingsControllerTest extends TestCase
         $this->assertEquals( 302, $response->getStatusCode() );
         $this->assertFalse( get_plugin_option( 'require_login', true ) );
     }
+    /**
+     * @test
+     */
+    public function it_updates_show_in_menu()
+    {
+        set_plugin_option( 'show_in_menu', 'off' );
+        $request = ServerRequestFactory::request('POST', '/admin.php?page=dt_home&tab=general', [
+            'dt_home_show_in_menu' => 'on',
+        ]);
+
+        $controller = container()->get( GeneralSettingsController::class );
+        $response = $controller->update( $request );
+        $this->assertEquals( 302, $response->getStatusCode() );
+        $this->assertTrue( get_plugin_option( 'show_in_menu', false ) );
+    }
+    /**
+     * @test
+     */
+    public function it_updates_show_in_menu_off()
+    {
+        set_plugin_option( 'show_in_menu', 'on' );
+        $request = ServerRequestFactory::request('POST', '/admin.php?page=dt_home&tab=general', [
+            'dt_home_show_in_menu' => 'off',
+        ]);
+
+        $controller = container()->get( GeneralSettingsController::class );
+        $response = $controller->update( $request );
+        $this->assertEquals( 302, $response->getStatusCode() );
+        $this->assertFalse( get_plugin_option( 'show_in_menu', true ) );
+    }
 }
