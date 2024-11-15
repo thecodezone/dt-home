14914b2428433be51c5b9ed77f8591a1a985c231
Muniraj <muni20002raj@gmail.com>
2024-11-15 20:04:30 +0530

    feature/admin-export-app-config

--- src/Services/Assets.php
+++ src/Services/Assets.php
@@ -377,6 +377,9 @@ class Assets
         wp_register_style( 'jquery-ui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css' );
         wp_enqueue_style( 'jquery-ui' );
 
+        wp_register_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css' );
+        wp_enqueue_style( 'font-awesome' );
+
         dt_theme_enqueue_style( 'material-font-icons-local', 'dt-core/dependencies/mdi/css/materialdesignicons.min.css', [] );
         wp_enqueue_style( 'material-font-icons', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.6.96/css/materialdesignicons.min.css' );
 
