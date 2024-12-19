3a7c1041f74eb4ddb47e454c2ac9062938f0ec98
Muniraj <muni20002raj@gmail.com>
2024-12-19 14:53:18 +0530

    Add Admin DT Home Analytics Permission field to the General tab

--- resources/views/settings/general.php
+++ resources/views/settings/general.php
@@ -11,40 +11,41 @@
  * @var string $dt_home_button_color
  * @var string $dt_home_show_in_menu
  * @var string $dt_home_file_upload
+ * @var string $dt_home_analytics_permission
  */
 $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
 ?>
 
 <form method="post" action="admin.php?page=dt_home&tab=general">
-	<?php wp_nonce_field( 'dt_admin_form_nonce' ); ?>
-	<div class="error-message" style="display:none;"></div>
-	<table class="widefat striped">
+    <?php wp_nonce_field( 'dt_admin_form_nonce' ); ?>
+    <div class="error-message" style="display:none;"></div>
+    <table class="widefat striped">
         <thead>
         <tr>
             <th colspan="3"> <?php esc_html_e( 'General Settings', 'dt-home' ); ?></th>
         </thead>
         <tbody>
-		<tr>
-			<td>
-				<label for="dt_home_require_login">
-					<input type="checkbox" id="dt_home_require_login"
-							name="dt_home_require_login" <?php checked( $dt_home_require_login ); ?>
-					>
-					<?php esc_html_e( 'Require users to login to access the home screen magic link?', 'dt-home' ); ?>
-				</label>
-			</td>
+        <tr>
+            <td>
+                <label for="dt_home_require_login">
+                    <input type="checkbox" id="dt_home_require_login"
+                           name="dt_home_require_login" <?php checked( $dt_home_require_login ); ?>
+                    >
+                    <?php esc_html_e( 'Require users to login to access the home screen magic link?', 'dt-home' ); ?>
+                </label>
+            </td>
 
-		</tr>
-		<tr>
-			<td>
-				<label for="dt_home_reset_apps">
-					<input type="checkbox" id="dt_home_reset_apps"
-							name="dt_home_reset_apps" <?php checked( $dt_home_reset_apps ); ?>
-					>
-					<?php esc_html_e( 'Allow users to reset their apps?', 'dt-home' ); ?>
-				</label>
-			</td>
-		</tr>
+        </tr>
+        <tr>
+            <td>
+                <label for="dt_home_reset_apps">
+                    <input type="checkbox" id="dt_home_reset_apps"
+                           name="dt_home_reset_apps" <?php checked( $dt_home_reset_apps ); ?>
+                    >
+                    <?php esc_html_e( 'Allow users to reset their apps?', 'dt-home' ); ?>
+                </label>
+            </td>
+        </tr>
         <tr>
             <td>
                 <label for="dt_home_use_capabilities">
@@ -66,16 +67,25 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
         </tr>
         <tr>
             <td>
-            <label for="dt_home_button_color" class="color-picker-lable">
-                <input type="color" id="dt_home_button_color" class="color-picker"
-                       name="dt_home_button_color" value="<?php echo esc_attr( $dt_home_button_color ); ?>"
-                >
-                <?php esc_html_e( 'Customize Add Button Color?', 'dt-home' ); ?>
-            </label>
+                <label for="dt_home_analytics_permission">
+                    <input type="checkbox" id="dt_home_analytics_permission"
+                           name="dt_home_analytics_permission" <?php checked( $dt_home_analytics_permission ); ?>>
+                    <?php esc_html_e( 'Enable this option to allow us to collect usage data to improve the app. Your data remains private and is not shared externally?', 'dt-home' ); ?>
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="dt_home_button_color" class="color-picker-lable">
+                    <input type="color" id="dt_home_button_color" class="color-picker"
+                           name="dt_home_button_color" value="<?php echo esc_attr( $dt_home_button_color ); ?>"
+                    >
+                    <?php esc_html_e( 'Customize Add Button Color?', 'dt-home' ); ?>
+                </label>
             </td>
         </tr>
         </tbody>
-	</table>
+    </table>
     <br>
     <table class="widefat striped">
         <thead>
@@ -84,28 +94,36 @@ $this->layout( 'layouts/settings', compact( 'tab', 'link', 'page_title' ) )
         </tr>
         </thead>
         <tbody>
-          <tr>
-              <td>
-                  <table class="widefat striped">
-                      <thead>
-                      <tr>
-                          <td><?php esc_html_e( 'Image', 'dt-home' ); ?></td>
-                          <td><?php esc_html_e( 'Image link (must be https)', 'dt-home' ); ?> </td>
-                          <td></td>
-                          <td></td>
-                      </tr>
-                      </thead>
-                      <tbody>
-                          <tr>
-                              <td style="background-color:#3f729b" id="image_preview"><img height="22px" src="<?php echo esc_url( $dt_home_file_upload ); ?>"></td>
-                              <td><input type="text" id="dt_home_file_upload" name="dt_home_custom_ministry_logo" value="<?php echo esc_url( $dt_home_file_upload ); ?>" pattern=".*\S+.*" title="<?php esc_attr_e( 'The name cannot be empty or just whitespace.', 'dt-home' ); ?>"></td>
-                              <td></td>
-                              <td><button class="button file-upload-display-uploader" id="upload_image_button"  style="margin-left:1%"><?php esc_html_e( 'Upload', 'dt-home' ); ?></button></td>
-                          </tr>
-                      </tbody>
-                  </table>
-              </td>
-          </tr>
+        <tr>
+            <td>
+                <table class="widefat striped">
+                    <thead>
+                    <tr>
+                        <td><?php esc_html_e( 'Image', 'dt-home' ); ?></td>
+                        <td><?php esc_html_e( 'Image link (must be https)', 'dt-home' ); ?> </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td style="background-color:#3f729b" id="image_preview"><img height="22px"
+                                                                                     src="<?php echo esc_url( $dt_home_file_upload ); ?>">
+                        </td>
+                        <td><input type="text" id="dt_home_file_upload" name="dt_home_custom_ministry_logo"
+                                   value="<?php echo esc_url( $dt_home_file_upload ); ?>" pattern=".*\S+.*"
+                                   title="<?php esc_attr_e( 'The name cannot be empty or just whitespace.', 'dt-home' ); ?>">
+                        </td>
+                        <td></td>
+                        <td>
+                            <button class="button file-upload-display-uploader" id="upload_image_button"
+                                    style="margin-left:1%"><?php esc_html_e( 'Upload', 'dt-home' ); ?></button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
         </tbody>
     </table>
     <br>
